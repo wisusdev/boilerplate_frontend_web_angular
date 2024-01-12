e8031258fcb44a8e56e0cddceaa0ab138c14dbeb
Jesus Avelar <wisusdev@gmail.com>
2024-01-12 11:26:33 -0600

    Add TranslateService and environment import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+import { environment } from '../environments/environment';
 
 @Component({
 	selector: 'app-root',
@@ -6,5 +8,5 @@ import {Component} from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	title = 'todoist';
+	title = environment.APP_NAME;
 }
