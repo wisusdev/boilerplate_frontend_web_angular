9e63f5d1385424685a2a837816d34346bbc7bb47
Jesus Avelar <wisusdev@gmail.com>
2024-01-17 22:35:32 -0600

    Refactor auth.guard.ts to use AuthUser instead of AuthService

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,13 +1,13 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
-import {AuthService} from "../services/auth.service";
 import {map, take} from "rxjs";
+import { AuthUser } from '../core/AuthUser';
 
 export const authGuard: CanActivateFn = (route, state) => {
-	const authService = inject(AuthService);
+	const authUser = inject(AuthUser);
 	const router = inject(Router);
 
-	return authService.status().pipe(take(1), map((isLoggedIn: boolean) => {
+	return authUser.status().pipe(take(1), map((isLoggedIn: boolean) => {
 		if (!isLoggedIn) {
 			router.createUrlTree(['/auth/login']);
 			return false;
