159adb7c5294fc8283ef38b11d63afbd922c7e58
Jesus Avelar <wisusdev@gmail.com>
2024-01-17 22:36:42 -0600

    Add error handling for email input in forgot password component

--- src/app/resources/views/auth/forgot-password/forgot-password.component.ts
+++ src/app/resources/views/auth/forgot-password/forgot-password.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../../../services/auth.service';
-import { ToastService } from "../../../../services/toast.service";
+import { HandleMessage } from 'src/app/core/HandleMessage';
+import { ErrorMessages } from 'src/app/interfaces/errors.interface';
 
 @Component({
 	selector: 'app-forgot-password',
@@ -9,9 +10,19 @@ import { ToastService } from "../../../../services/toast.service";
 })
 export class ForgotPasswordComponent implements OnInit {
 
-	constructor(private formBuilder: FormBuilder, private authService: AuthService, private toast: ToastService) { }
+	constructor(private formBuilder: FormBuilder, private authService: AuthService, private handleMessage: HandleMessage) { }
 
 	formForgotPassword!: FormGroup;
+
+	errorMessages: ErrorMessages = {
+		email: ''
+	};
+
+	resetErrorMessages() {
+		this.errorMessages = {
+			email: ''
+		};
+	}
 
 	ngOnInit() {
 		this.formForgotPassword = this.formBuilder.group({
@@ -25,15 +36,8 @@ export class ForgotPasswordComponent implements OnInit {
 
 	onSubmit() {
 		this.authService.forgotPassword(this.formForgotPassword.value).subscribe(
-			(response) => {
-				console.log(response);
-				this.formForgotPassword.reset();
-				this.toast.show({ message: 'We have emailed your password reset link!', classname: 'bg-success text-light', delay: 5000 });
-			},
-			(error) => {
-				console.log(error);
-				this.toast.show({ message: 'Error resetting password', classname: 'bg-danger text-light', delay: 5000 });
-			}
+			(response) => this.handleMessage.handleResponse(response, this.formForgotPassword, '/auth/login'),
+			(error) => this.handleMessage.handleError(error, this.errorMessages)
 		);
 	}
 }
