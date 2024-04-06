dd9cc91dc7061b790136dea0f4466430a6c8c967
Jesús Avelar <wisusdev@gmail.com>
2024-04-05 22:48:38 -0600

    Agregar archivo de configuración de la API

---
+++ src/app/config/Api.ts
@@ -0,0 +1,10 @@
+export const Api = {
+	API_URL: 'http://localhost:8000/api',
+
+	API_VERSION: '/v1',
+
+	HEADERS: {
+		'Content-Type': 'application/vnd.api+json',
+		'Accept': 'application/vnd.api+json',
+	},
+}
