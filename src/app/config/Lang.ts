0581528887a97098f748a34e76d6b1ddd956d178
Jesús Avelar <wisusdev@gmail.com>
2024-04-06 09:51:56 -0600

    Actualizar archivo de configuración de Lang.ts

---
+++ src/app/config/Lang.ts
@@ -0,0 +1,14 @@
+export const Lang = {
+	availableLang: {
+		'es': {
+			'name': 'Español',
+			'default': true,
+			'dir': 'ltr'
+		},
+		'en': {
+			'name': 'English',
+			'default': false,
+			'dir': 'ltr',
+		}
+	},
+}
