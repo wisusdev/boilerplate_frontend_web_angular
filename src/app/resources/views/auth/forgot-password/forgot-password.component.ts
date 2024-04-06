116271fa16251b5572d418215a9e3d66f27016d3
Jesús Avelar <wisusdev@gmail.com>
2024-04-05 22:13:19 -0600

    Actualizar importación de Auth en navbar.component.ts

--- src/app/resources/views/auth/forgot-password/forgot-password.component.ts
+++ src/app/resources/views/auth/forgot-password/forgot-password.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../../../../core/Services/auth.service';
+import { AuthService } from '../../../../core/Services/Auth.service';
 import { Handle } from '../../../../core/Exceptions/Handle';
-import { ErrorMessages } from '../../../../core/Interfaces/errors.interface';
+import { ErrorMessages } from '../../../../core/Interfaces/Errors.interface';
 
 @Component({
 	selector: 'app-forgot-password',
