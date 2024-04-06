52a0d7b8d7b6cbc2d50ecfe699b9eb7eb70a38f4
Jesús Avelar <wisusdev@gmail.com>
2024-04-05 22:00:10 -0600

    Agregar guardia de autenticación en Auth.guard.ts

---
+++ src/app/core/Guards/Auth.guard.ts
@@ -0,0 +1,17 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {map, take} from "rxjs";
+import { Auth } from '../../config/Auth';
+
+export const authGuard: CanActivateFn = (route, state) => {
+	const authUser = inject(Auth);
+	const router = inject(Router);
+
+	return authUser.status().pipe(take(1), map((isLoggedIn: boolean) => {
+		if (!isLoggedIn) {
+			router.createUrlTree(['/auth/login']);
+			return false;
+		}
+		return true;
+	}));
+};
