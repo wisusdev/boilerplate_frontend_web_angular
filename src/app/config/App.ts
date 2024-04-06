09d8f99e6529eb3ab17633cd7df3d29352b5880a
Jesús Avelar <wisusdev@gmail.com>
2024-04-05 21:58:51 -0600

    Agregar archivo de configuración de la aplicación

---
+++ src/app/config/App.ts
@@ -0,0 +1,13 @@
+export const app = {
+	/* Application Name */
+	NAME: 'Angular App',
+
+	/* Backend API URL */
+	API_URL: 'http://localhost:8000/api',
+
+	/* Backend API Version */
+	API_VERSION: '/v1',
+
+	/* Application locale */
+	LOCALE: 'en',
+}
