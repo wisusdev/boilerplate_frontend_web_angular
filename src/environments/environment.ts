a0b0deffbc69cbaaf585c2f9caf8ea70146d4a88
Jesús Avelar <wisusdev@gmail.com>
2024-04-05 22:07:37 -0600

    Eliminar archivo de entorno no utilizado

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    APP_NAME: 'todolist',
-    API_URL: 'http://localhost:8000/api',
-    API_VERSION: '/v1',
-    APP_VERSION: '1.0.0',
-    APP_YEAR: '2021'
-};
