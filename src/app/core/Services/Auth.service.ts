ce7581c931d173e93d963e82b08a99597179f80c
Jesús Avelar <wisusdev@gmail.com>
2024-04-06 08:53:12 -0600

    Actualizar importación de Auth en navbar.component.ts

--- src/app/core/Services/Auth.service.ts
+++ src/app/core/Services/Auth.service.ts
@@ -5,65 +5,66 @@ import { Observable } from "rxjs";
 import { catchError } from "rxjs/operators";
 import { authUserModel } from '../Interfaces/Token.interface';
 import { Handle } from "../Exceptions/Handle";
-import {Api} from "../../config/Api";
+import { Api } from "../../config/Api";
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthService {
-    private _apiUri = Api.API_URL + Api.API_VERSION;
-    private _apiUriLogin = this._apiUri + '/auth/login';
-    private _apiUriLogout = this._apiUri + '/auth/logout';
-    private _apiUriRefresh = this._apiUri + '/auth/refresh';
-    private _apiUriRegister = this._apiUri + '/auth/register';
-    private _apiUriForgotPassword = this._apiUri + '/auth/forgot-password';
-    private _apiUriResetPassword = this._apiUri + '/auth/reset-password';
+	private _apiUri = Api.API_URL + Api.API_VERSION;
+	private _apiUriLogin = this._apiUri + '/auth/login';
+	private _apiUriLogout = this._apiUri + '/auth/logout';
+	private _apiUriRefresh = this._apiUri + '/auth/refresh';
+	private _apiUriRegister = this._apiUri + '/auth/register';
+	private _apiUriForgotPassword = this._apiUri + '/auth/forgot-password';
+	private _apiUriResetPassword = this._apiUri + '/auth/reset-password';
 
-    constructor(private httpClient: HttpClient, private handleMessage: Handle) {}
+	constructor(private httpClient: HttpClient, private handleMessage: Handle) {
+	}
 
-    httpHeaders: HttpHeaders = new HttpHeaders(Api.HEADERS);
+	httpHeaders: HttpHeaders = new HttpHeaders(Api.HEADERS);
 
-    register(data: UserInterface): Observable<object> {
-        return this.httpClient.post(`${this._apiUriRegister}`, data, {
-            headers: this.httpHeaders,
-        }).pipe(catchError(this.handleMessage.errorHandle));
-    }
+	register(data: UserInterface): Observable<object> {
+		return this.httpClient.post(`${this._apiUriRegister}`, data, {
+			headers: this.httpHeaders,
+		}).pipe(catchError(this.handleMessage.errorHandle));
+	}
 
-    login(data: UserInterface): Observable<authUserModel> {
-        return this.httpClient.post<authUserModel>(`${this._apiUriLogin}`, data, {
-            headers: this.httpHeaders,
-        }).pipe(catchError(this.handleMessage.errorHandle));
-    }
+	login(data: UserInterface): Observable<authUserModel> {
+		return this.httpClient.post<authUserModel>(`${this._apiUriLogin}`, data, {
+			headers: this.httpHeaders,
+		}).pipe(catchError(this.handleMessage.errorHandle));
+	}
 
-    logout(): Observable<object> {
-        const token: string | null = localStorage.getItem('token');
-		this.httpHeaders = new HttpHeaders({...Api.HEADERS, 'Authorization': `Bearer ${token}`});
-        return this.httpClient.post(`${this._apiUriLogout}`, {}, {
-            headers: this.httpHeaders,
-        }).pipe(catchError(this.handleMessage.errorHandle));
-    }
+	logout(): Observable<object> {
+		const token: string | null = localStorage.getItem('token');
+		this.httpHeaders = new HttpHeaders({ ...Api.HEADERS, 'Authorization': `Bearer ${token}` });
+		return this.httpClient.post(`${this._apiUriLogout}`, {}, {
+			headers: this.httpHeaders,
+		}).pipe(catchError(this.handleMessage.errorHandle));
+	}
 
-    refreshToken(): Observable<object> {
-        return this.httpClient.post(`${this._apiUriRefresh}`, {
-            headers: this.httpHeaders,
-        }).pipe(catchError(this.handleMessage.errorHandle));
-    }
+	refreshToken(): Observable<object> {
+		return this.httpClient.post(`${this._apiUriRefresh}`, {
+			headers: this.httpHeaders,
+		}).pipe(catchError(this.handleMessage.errorHandle));
+	}
 
-    forgotPassword(data: UserInterface): Observable<object> {
-        return this.httpClient.post(`${this._apiUriForgotPassword}`, data, {
-            headers: this.httpHeaders,
-        }).pipe(catchError(this.handleMessage.errorHandle));
-    }
+	forgotPassword(data: UserInterface): Observable<object> {
+		return this.httpClient.post(`${this._apiUriForgotPassword}`, data, {
+			headers: this.httpHeaders,
+		}).pipe(catchError(this.handleMessage.errorHandle));
+	}
 
-    resetPassword(token: string, password: string, password_confirmation: string): Observable<object> {
-        const data = {
-            token: token,
-            password: password,
-            password_confirmation: password_confirmation,
-        };
+	resetPassword(token: string, password: string, password_confirmation: string): Observable<object> {
+		const data = {
+			token: token,
+			password: password,
+			password_confirmation: password_confirmation,
+		};
 
-        return this.httpClient.post(`${this._apiUriResetPassword}`, data, {
-            headers: this.httpHeaders,
-        }).pipe(catchError(this.handleMessage.errorHandle));
-    }
+		return this.httpClient.post(`${this._apiUriResetPassword}`, data, {
+			headers: this.httpHeaders,
+		}).pipe(catchError(this.handleMessage.errorHandle));
+	}
 }
