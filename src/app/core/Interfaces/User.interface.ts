985d8b8c63236728e09695f2079b4da3a93f0a3a
Jesús Avelar <wisusdev@gmail.com>
2024-04-05 22:00:30 -0600

    Agregar interfaz de usuario en User.interface.ts

---
+++ src/app/core/Interfaces/User.interface.ts
@@ -0,0 +1,8 @@
+export interface UserInterface {
+    username: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+}
