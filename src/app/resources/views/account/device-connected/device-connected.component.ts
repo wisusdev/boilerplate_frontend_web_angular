1dbcf8d6c9205ae3bda1d8a6e687523e4010eb1f
Jesús Avelar <wisusdev@gmail.com>
2024-04-13 07:50:25 -0600

    Agregar componente de dispositivo conectado y su plantilla HTML

---
+++ src/app/resources/views/account/device-connected/device-connected.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {TranslateModule} from "@ngx-translate/core";
+import {AppModule} from "../../../../app.module";
+
+@Component({
+	selector: 'app-device-connected',
+	templateUrl: './device-connected.component.html',
+})
+export class DeviceConnectedComponent {
+
+}
