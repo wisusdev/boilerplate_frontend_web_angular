c229abd0bf0c24d8659d505e4fb4d91804ee593c
Jesús Avelar <wisusdev@gmail.com>
2024-04-13 07:51:58 -0600

    Agregar módulo de enrutamiento de autenticación con nuevos componentes

---
+++ src/app/resources/views/auth/auth-routing.module.ts
@@ -0,0 +1,21 @@
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+import {guestGuard} from "../../../data/Guards/Guest.guard";
+import {RegisterComponent} from "./register/register.component";
+import {ForgotPasswordComponent} from "./forgot-password/forgot-password.component";
+import {ResetPasswordComponent} from "./reset-password/reset-password.component";
+import {NgModule} from "@angular/core";
+
+const routes: Routes = [
+	{ path: '', redirectTo: 'auth/login', pathMatch: 'full'},
+	{ path: 'auth/login', component: LoginComponent, canActivate: [guestGuard]},
+	{ path: 'auth/register', component: RegisterComponent, canActivate: [guestGuard] },
+	{ path: 'auth/forgot-password', component: ForgotPasswordComponent, canActivate: [guestGuard]},
+	{ path: 'auth/reset-password', component: ResetPasswordComponent, canActivate: [guestGuard]},
+];
+
+@NgModule({
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
+})
+export class AuthRoutingModule {}
