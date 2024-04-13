979bf99650cb8ffd4e8c9c1d9722e6743b59617d
Jesús Avelar <wisusdev@gmail.com>
2024-04-13 07:50:04 -0600

    Agregar componente de cambio de contraseña y su plantilla HTML

---
+++ src/app/resources/views/account/change-password/change-password.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+})
+export class ChangePasswordComponent {
+
+}
