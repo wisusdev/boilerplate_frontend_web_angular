d2d758af426c199838116011c76b711c71ea9943
Jesús Avelar <wisusdev@gmail.com>
2024-04-13 07:48:27 -0600

    Actualizar módulo de enrutamiento de la aplicación con nuevas importaciones y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from "./resources/views/auth/login/login.component";
-import {RegisterComponent} from "./resources/views/auth/register/register.component";
-import {ForgotPasswordComponent} from "./resources/views/auth/forgot-password/forgot-password.component";
-import {ProfileComponent} from "./resources/views/profile/profile.component";
-import {authGuard} from "./data/Guards/Auth.guard";
-import {ResetPasswordComponent} from "./resources/views/auth/reset-password/reset-password.component";
-import {guestGuard} from './data/Guards/Guest.guard';
+import {AccountRoutingModule} from "./resources/views/account/account-routing.module";
+import {AuthRoutingModule} from "./resources/views/auth/auth-routing.module";
 
 const routes: Routes = [
-	{ path: '', redirectTo: 'auth/login', pathMatch: 'full'},
-	{ path: 'auth/login', component: LoginComponent, canActivate: [guestGuard]},
-	{ path: 'auth/register', component: RegisterComponent, canActivate: [guestGuard] },
-	{ path: 'auth/forgot-password', component: ForgotPasswordComponent, canActivate: [guestGuard]},
-	{ path: 'auth/reset-password', component: ResetPasswordComponent, canActivate: [guestGuard]},
-	{ path: 'profile', component: ProfileComponent, canActivate: [authGuard]},
+
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [
+		RouterModule.forRoot(routes),
+		AccountRoutingModule,
+		AuthRoutingModule
+	],
 	exports: [RouterModule]
 })
 export class AppRoutingModule {
