0780b6032840f26676ad69c28b6160d6746047cc
Jesús Avelar <wisusdev@gmail.com>
2024-04-13 07:49:51 -0600

    Agregar componente de lista de menú de cuenta y su plantilla HTML

---
+++ src/app/resources/views/account/account-menu-list/account-menu-list.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-account-menu-list',
+	templateUrl: './account-menu-list.component.html'
+})
+export class AccountMenuListComponent {
+
+}
