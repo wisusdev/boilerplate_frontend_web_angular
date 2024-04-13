3d38fecec36bfbbbbd1708dfbce9af87bf26c74b
Jesús Avelar <wisusdev@gmail.com>
2024-04-13 07:48:13 -0600

    Actualizar módulo de la aplicación con nuevas importaciones y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
+import {NgOptimizedImage} from "@angular/common";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from "./resources/views/shared/navbar/navbar.component";
-import { ToastComponent } from "./resources/components/toast/toast.component";
-import { ThemeComponent } from "./resources/components/theme/theme.component";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
-import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
-import { TranslateHttpLoader } from "@ngx-translate/http-loader";
-import { ResetPasswordComponent } from './resources/views/auth/reset-password/reset-password.component';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { LoginComponent } from "./resources/views/auth/login/login.component";
-import { RegisterComponent } from "./resources/views/auth/register/register.component";
-import { ForgotPasswordComponent } from "./resources/views/auth/forgot-password/forgot-password.component";
-import { ProfileComponent } from "./resources/views/profile/profile.component";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from "./resources/views/shared/navbar/navbar.component";
+import {ToastComponent} from "./resources/components/toast/toast.component";
+import {ThemeComponent} from "./resources/components/theme/theme.component";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {ResetPasswordComponent} from './resources/views/auth/reset-password/reset-password.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {LoginComponent} from "./resources/views/auth/login/login.component";
+import {RegisterComponent} from "./resources/views/auth/register/register.component";
+import {ForgotPasswordComponent} from "./resources/views/auth/forgot-password/forgot-password.component";
+import {ProfileComponent} from "./resources/views/account/profile/profile.component";
+import {AccountMenuListComponent} from "./resources/views/account/account-menu-list/account-menu-list.component";
+import {ChangePasswordComponent} from "./resources/views/account/change-password/change-password.component";
+import {DeviceConnectedComponent} from "./resources/views/account/device-connected/device-connected.component";
+
 import {
 	NgbCollapse,
 	NgbDropdown,
@@ -31,7 +36,10 @@ import {
 		ForgotPasswordComponent,
 		ResetPasswordComponent,
 		NavbarComponent,
-		ProfileComponent
+		ProfileComponent,
+		AccountMenuListComponent,
+		ChangePasswordComponent,
+		DeviceConnectedComponent
 	],
 	imports: [
 		BrowserModule,
@@ -52,10 +60,14 @@ import {
 		NgbDropdownToggle,
 		NgbDropdownItem,
 		NgbDropdownMenu,
-		NgbCollapse
+		NgbCollapse,
+		NgOptimizedImage,
 	],
 	providers: [
 		provideClientHydration()
+	],
+	exports: [
+		AccountMenuListComponent
 	],
 	bootstrap: [AppComponent]
 })
