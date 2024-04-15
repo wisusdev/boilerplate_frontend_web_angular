25ff21c180831771e0f34829e4538c077e586296
Jesús Avelar <wisusdev@gmail.com>
2024-04-15 17:41:23 -0600

    Actualizar Auth.ts con métodos estáticos para obtener el ID de usuario y el token de acceso

--- src/app/data/Providers/Auth.ts
+++ src/app/data/Providers/Auth.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import {Injectable} from "@angular/core";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
 	providedIn: 'root'
@@ -19,5 +19,24 @@ export class Auth {
 		}
 
 		return this.isLoggedIn.asObservable();
+	}
+
+	static userId() {
+
+		if (typeof localStorage !== 'undefined') {
+			const userId: string | null = localStorage.getItem('user_key');
+			return userId;
+		}
+
+		return null;
+	}
+
+	static token() {
+		if (typeof localStorage !== 'undefined') {
+			const access_token: string | null = localStorage.getItem('access_token');
+			return access_token;
+		}
+
+		return null;
 	}
 }
