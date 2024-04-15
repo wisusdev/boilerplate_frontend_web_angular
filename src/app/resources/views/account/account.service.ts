24490d122017021a0e1e89f91e909edd58890bc1
Jesús Avelar <wisusdev@gmail.com>
2024-04-15 17:43:02 -0600

    Actualizar servicio de cuenta para incluir cambio de contraseña

--- src/app/resources/views/account/account.service.ts
+++ src/app/resources/views/account/account.service.ts
@@ -5,6 +5,7 @@ import {catchError, Observable} from "rxjs";
 import {Handle} from "../../../data/Exceptions/Handle";
 import {ProfileUpdateInterface} from "../../../data/Interfaces/Account/ProfileUpdate.interface";
 import {profileUpdateResponse} from "../../../data/Interfaces/Account/ProfileUpdateResponse.interface";
+import { Auth } from 'src/app/data/Providers/Auth';
 
 @Injectable({
 	providedIn: 'root'
@@ -12,6 +13,7 @@ import {profileUpdateResponse} from "../../../data/Interfaces/Account/ProfileUpd
 export class AccountService {
 	private _apiUri: string = Api.api_url_v1;
 	private _apiUriAccount: string = this._apiUri + '/account/profile';
+	private _apiUriChangePassword: string = this._apiUri + '/account/change-password';
 
 	httpHeaders: HttpHeaders = new HttpHeaders(Api.headers);
 
@@ -19,17 +21,22 @@ export class AccountService {
 	}
 
 	getInfoProfile(): Observable<object> {
-		const token: string | null = localStorage.getItem('access_token');
-		this.httpHeaders = new HttpHeaders({ ...Api.headers, 'Authorization': `Bearer ${token}` });
+		this.httpHeaders = new HttpHeaders({ ...Api.headers, 'Authorization': `Bearer ${Auth.token()}` });
 		return this.httpClient.get(`${this._apiUriAccount}`, {
 			headers: this.httpHeaders,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	updateProfile(data: ProfileUpdateInterface): Observable<profileUpdateResponse> {
-		const token: string | null = localStorage.getItem('access_token');
-		this.httpHeaders = new HttpHeaders({ ...Api.headers, 'Authorization': `Bearer ${token}` });
+		this.httpHeaders = new HttpHeaders({ ...Api.headers, 'Authorization': `Bearer ${Auth.token()}` });
 		return this.httpClient.patch<profileUpdateResponse>(`${this._apiUriAccount}`, data, {
+			headers: this.httpHeaders,
+		}).pipe(catchError(this.handleMessage.errorHandle));
+	}
+
+	changePassword(data: any): Observable<any> {
+		this.httpHeaders = new HttpHeaders({ ...Api.headers, 'Authorization': `Bearer ${Auth.token()}` });
+		return this.httpClient.patch(`${this._apiUriChangePassword}`, data, {
 			headers: this.httpHeaders,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
