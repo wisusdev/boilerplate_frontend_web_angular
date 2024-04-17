b03de9735627850bafb42058adc612bfdd496371
Jesus Avelar <wisusdev@gmail.com>
2024-04-16 21:13:24 -0600

    Actualizar componente de dispositivo conectado con correcciones y mejoras en la interfaz de usuario

--- src/app/resources/views/account/device-connected/device-connected.component.ts
+++ src/app/resources/views/account/device-connected/device-connected.component.ts
@@ -1,12 +1,43 @@
-import {Component} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {TranslateModule} from "@ngx-translate/core";
-import {AppModule} from "../../../../app.module";
+import {Component, OnInit} from '@angular/core';
+import {AccountService} from "../account.service";
+import {ToastService} from "../../../../data/Services/Toast.service";
+import {Handle} from "../../../../data/Exceptions/Handle";
+import {catchError, tap} from "rxjs";
 
 @Component({
 	selector: 'app-device-connected',
 	templateUrl: './device-connected.component.html',
 })
-export class DeviceConnectedComponent {
+export class DeviceConnectedComponent implements OnInit {
+	constructor(private accountService: AccountService, private toast: ToastService, private handleMessage: Handle) {}
 
+	devices: any = [];
+	totalPages: number = 0;
+	pageNumber: number = 1;
+	pages: number[] = [];
+
+	ngOnInit(): void {
+		this.getDevices();
+	}
+
+	getDevices() {
+		let data = {
+			page: this.pageNumber
+		}
+
+		this.accountService.getDeviceAuthList(data).pipe(
+			tap((data: any) => {
+				this.devices = data.data;
+				this.totalPages = data.data.last_page;
+				this.pageNumber = data.data.current_page;
+				this.pages = Array.from({length: this.totalPages}, (_, i) => i + 1);
+			}),
+			catchError(this.handleMessage.errorHandle)
+		).subscribe();
+	}
+
+	setPage(pageNumber: number): void {
+		this.pageNumber = pageNumber;
+		this.getDevices();
+	}
 }
