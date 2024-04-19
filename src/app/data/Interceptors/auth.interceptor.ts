5a7064f73efcdf1c6a6adb9ce092f91eb15a5fb5
Jesús Avelar <wisusdev@gmail.com>
2024-04-18 21:50:58 -0600

    Agregar interceptor de autenticación en las solicitudes HTTP

---
+++ src/app/data/Interceptors/auth.interceptor.ts
@@ -0,0 +1,28 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import {catchError} from "rxjs";
+import {inject} from "@angular/core";
+import {Router} from "@angular/router";
+import {app} from "../../config/App";
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+	const router = inject(Router);
+
+	const access_token: string | null = localStorage.getItem('access_token');
+
+	const authReq = req.clone({
+		setHeaders: {
+			Authorization: `Bearer ${access_token}`
+		}
+	});
+
+	return next(authReq).pipe(
+		catchError((error) => {
+			console.log(error.status);
+			if (error.status === 401) {
+				localStorage.clear();
+				router.navigate([app.redirectLogout]);
+			}
+			throw 'Error';
+		})
+	);
+};
