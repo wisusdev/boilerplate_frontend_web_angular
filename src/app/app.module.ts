0846b1c219e92d4bd222ae4e50fba46f3781a045
Jesús Avelar <wisusdev@gmail.com>
2024-04-18 21:53:11 -0600

    Actualizar importación de AuthService en el componente de barra de navegación compartida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
-import {NgOptimizedImage} from "@angular/common";
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { NgOptimizedImage } from "@angular/common";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {NavbarComponent} from "./resources/views/shared/navbar/navbar.component";
-import {ToastComponent} from "./resources/components/toast/toast.component";
-import {ThemeComponent} from "./resources/components/theme/theme.component";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
-import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-import {ResetPasswordComponent} from './resources/views/auth/reset-password/reset-password.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {LoginComponent} from "./resources/views/auth/login/login.component";
-import {RegisterComponent} from "./resources/views/auth/register/register.component";
-import {ForgotPasswordComponent} from "./resources/views/auth/forgot-password/forgot-password.component";
-import {ProfileComponent} from "./resources/views/account/profile/profile.component";
-import {AccountMenuListComponent} from "./resources/views/account/account-menu-list/account-menu-list.component";
-import {ChangePasswordComponent} from "./resources/views/account/change-password/change-password.component";
-import {DeviceConnectedComponent} from "./resources/views/account/device-connected/device-connected.component";
-import {ConfirmationDialogComponent} from "./resources/components/confirmation-dialog/confirmation-dialog.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from "./resources/views/shared/navbar/navbar.component";
+import { ToastComponent } from "./resources/components/toast/toast.component";
+import { ThemeComponent } from "./resources/components/theme/theme.component";
+import { HttpClient, HttpClientModule, provideHttpClient, withFetch, withInterceptors } from "@angular/common/http";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { ResetPasswordComponent } from './resources/views/auth/reset-password/reset-password.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { LoginComponent } from "./resources/views/auth/login/login.component";
+import { RegisterComponent } from "./resources/views/auth/register/register.component";
+import { ForgotPasswordComponent } from "./resources/views/auth/forgot-password/forgot-password.component";
+import { ProfileComponent } from "./resources/views/account/profile/profile.component";
+import { AccountMenuListComponent } from "./resources/views/account/account-menu-list/account-menu-list.component";
+import { ChangePasswordComponent } from "./resources/views/account/change-password/change-password.component";
+import { DeviceConnectedComponent } from "./resources/views/account/device-connected/device-connected.component";
+import { ConfirmationDialogComponent } from "./resources/components/confirmation-dialog/confirmation-dialog.component";
+import { authInterceptor } from './data/Interceptors/auth.interceptor';
 
 import {
 	NgbCollapse,
@@ -68,7 +69,12 @@ import {
 		NgbPaginationModule
 	],
 	providers: [
-		provideClientHydration()
+		provideHttpClient(
+			withFetch(),
+			withInterceptors([
+				authInterceptor
+			])
+		)
 	],
 	exports: [
 		AccountMenuListComponent
