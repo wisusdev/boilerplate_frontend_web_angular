d79dc79f1678583fa837090d69770decc67641f6
Jesús Avelar <wisusdev@gmail.com>
2024-04-19 22:41:14 -0600

    Agregar enlace para eliminar cuenta en el menú de la cuenta

--- src/app/resources/views/account/account-menu-list/account-menu-list.component.ts
+++ src/app/resources/views/account/account-menu-list/account-menu-list.component.ts
@@ -1,7 +1,15 @@
 import {Component} from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
 	selector: 'app-account-menu-list',
+	standalone: true,
+	imports: [
+		TranslateModule,
+		RouterLink,
+		RouterLinkActive
+	],
 	templateUrl: './account-menu-list.component.html'
 })
 export class AccountMenuListComponent {
