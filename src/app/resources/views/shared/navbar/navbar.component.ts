a423bed839c5600f3dc301075a8388856f46fba8
Jesús Avelar <wisusdev@gmail.com>
2024-04-24 13:59:09 -0600

    Actualizar importación de AuthService en el componente de barra de navegación compartida

--- src/app/resources/views/shared/navbar/navbar.component.ts
+++ src/app/resources/views/shared/navbar/navbar.component.ts
@@ -1,24 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router";
-import { TranslateService } from "@ngx-translate/core";
-import { AuthService } from '../../auth/auth.service';
-import { Auth } from '../../../../data/Providers/Auth';
-import { app } from "../../../../config/App";
-import { Lang } from "../../../../config/Lang";
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {AuthService} from '../../auth/auth.service';
+import {Auth} from '../../../../data/Providers/Auth';
+import {app} from "../../../../config/App";
+import {Lang} from "../../../../config/Lang";
+import {
+	NgbCollapse,
+	NgbDropdown,
+	NgbDropdownItem,
+	NgbDropdownMenu,
+	NgbDropdownToggle
+} from '@ng-bootstrap/ng-bootstrap';
+import {NgForOf} from "@angular/common";
 
 @Component({
 	selector: 'app-navbar',
+	standalone: true,
+	imports: [
+		NgbCollapse,
+		RouterLink,
+		TranslateModule,
+		NgbDropdown,
+		NgbDropdownMenu,
+		NgbDropdownToggle,
+		NgbDropdownItem,
+		NgForOf
+	],
 	templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
-	collapsed: boolean = true;
 	public loggedIn: boolean = false;
 	public isMenuCollapsed: boolean = true;
 	public appName: string = app.name;
 	public availableLangNavbarArray: any[] = [];
 
 	constructor(private router: Router, public translate: TranslateService, private authService: AuthService, private authUser: Auth) {
-		this.availableLangNavbarArray = Object.entries(Lang.availableLang).map(([key, value]) => ({ key, ...value }));
+		this.availableLangNavbarArray = Object.entries(Lang.availableLang).map(([key, value]) => ({key, ...value}));
 		translate.addLangs(Object.keys(Lang.availableLang));
 		for (let lang of this.availableLangNavbarArray) {
 			if (lang.default) {
