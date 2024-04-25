82e59ca441071a7ef14a3f1627832f52406ea16a
Jesús Avelar <wisusdev@gmail.com>
2024-04-25 09:39:47 -0600

    Agregar componente de roles en roles.component.ts y roles.component.html

---
+++ src/app/resources/views/settings/roles/roles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [],
+  templateUrl: './roles.component.html'
+})
+export class RolesComponent {
+
+}
