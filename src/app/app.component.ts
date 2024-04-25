37ca824a777726723133379f17dc402500d46552
Jesús Avelar <wisusdev@gmail.com>
2024-04-25 09:42:03 -0600

    Actualizar importación de estilos en main.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import {Component, TemplateRef, ViewChild} from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import { Auth } from './data/Providers/Auth';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	
+	public loggedIn: boolean = false;
 
+	constructor(private authUser: Auth) { }
+	
+	ngOnInit() {
+		this.authUser.status().pipe().subscribe((status: boolean) => {
+			this.loggedIn = status;
+		});
+	}
 }
