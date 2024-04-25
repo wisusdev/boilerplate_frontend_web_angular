7111237a538f19f31e6f9b17573dabd9542d3e35
Jesús Avelar <wisusdev@gmail.com>
2024-04-25 09:41:42 -0600

    Agregar módulo de configuración de rutas en settings-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {AccountRoutingModule} from "./resources/views/account/account-routing.module";
-import {AuthRoutingModule} from "./resources/views/auth/auth-routing.module";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AccountRoutingModule } from "./resources/views/account/account-routing.module";
+import { AuthRoutingModule } from "./resources/views/auth/auth-routing.module";
+import { SettingsRoutingModule } from "./resources/views/settings/settings-routing.module";
+import { HomeComponent } from './resources/views/home/home.component';
+import { authGuard } from './data/Guards/Auth.guard';
 
 const routes: Routes = [
-	{ path: '', redirectTo: 'auth/login', pathMatch: 'full' }
+	{ path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+	{ path: 'home', component: HomeComponent, canActivate: [authGuard] }
 ];
 
 @NgModule({
 	imports: [
 		RouterModule.forRoot(routes),
 		AccountRoutingModule,
-		AuthRoutingModule
+		AuthRoutingModule,
+		SettingsRoutingModule
 	],
 	exports: [RouterModule]
 })
