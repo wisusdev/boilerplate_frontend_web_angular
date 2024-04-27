9bb64755e71c6aaa3eeeb8aac71354610fd702dd
Jesús Avelar <wisusdev@gmail.com>
2024-04-26 23:06:37 -0600

    Agregar componente de índice de usuario y su lógica

---
+++ src/app/resources/views/settings/users/index/indexUser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './indexUser.component.html',
+})
+export class IndexUserComponent {
+
+}
