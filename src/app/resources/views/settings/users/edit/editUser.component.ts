04079b5466a5164b5f095cc813df8daa0f55df18
Jesús Avelar <wisusdev@gmail.com>
2024-04-26 23:06:25 -0600

    Agregar componente de edición de usuario y su lógica

---
+++ src/app/resources/views/settings/users/edit/editUser.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+	selector: 'app-edit',
+	standalone: true,
+	imports: [],
+	templateUrl: './editUser.component.html',
+})
+export class EditUserComponent implements OnInit {
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+}
