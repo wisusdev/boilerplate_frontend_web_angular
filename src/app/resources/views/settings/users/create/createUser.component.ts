d0a56bbd03d7f2a856d1548d6566a7a1b84b579f
Jesús Avelar <wisusdev@gmail.com>
2024-04-26 23:06:16 -0600

    Agregar componente de creación de usuario y su lógica

---
+++ src/app/resources/views/settings/users/create/createUser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './createUser.component.html',
+})
+export class CreateUserComponent {
+
+}
