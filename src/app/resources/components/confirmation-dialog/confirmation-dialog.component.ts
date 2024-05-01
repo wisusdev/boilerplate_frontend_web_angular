501ca393218ae7195eeb53650daf194e287330b3
Jesús Avelar <wisusdev@gmail.com>
2024-05-01 14:18:03 -0600

    Actualizar traducciones en ConfirmationDialogComponent

--- src/app/resources/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/resources/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,18 +1,21 @@
 import {Component, Input} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
 	selector: 'app-confirmation-dialog',
 	templateUrl: './confirmation-dialog.component.html',
 })
 export class ConfirmationDialogComponent {
+	constructor(
+		public activeModal: NgbActiveModal,
+		private translate: TranslateService,
+	) { }
 
-	@Input() title!: string;
-	@Input() message!: string;
-	@Input() confirmText!: string;
-	@Input() cancelText!: string;
-
-	constructor(public activeModal: NgbActiveModal) { }
+	@Input() title: string = this.translate.instant('logout');
+	@Input() message: string = this.translate.instant('confirmAction');
+	@Input() confirmText: string = this.translate.instant('yesConfirm');
+	@Input() cancelText: string = this.translate.instant('noCancel');
 
 	public confirm() {
 		this.activeModal.close(true);
