7b42fa25865573bae5752d98f98b2116ffb29896
Jesús Avelar <wisusdev@gmail.com>
2024-05-01 14:26:39 -0600

    Actualizar componente ShowUserComponent en show.component.html y show.component.ts

--- src/app/resources/views/settings/settings-routing.module.ts
+++ src/app/resources/views/settings/settings-routing.module.ts
@@ -7,10 +7,12 @@ import {CreateUserComponent} from "./users/create/createUser.component";
 import {EditUserComponent} from "./users/edit/editUser.component";
 import {CreateRolComponent} from "./roles/create/createRol.component";
 import {EditRolComponent} from "./roles/edit/editRol.component";
+import { ShowUserComponent } from "./users/show/show.component";
 
 const router: Routes = [
 	{path: 'settings/users', component: IndexUserComponent, canActivate: [authGuard]},
 	{path: 'settings/users/create', component: CreateUserComponent, canActivate: [authGuard]},
+	{path: 'settings/users/show/:id', component: ShowUserComponent, canActivate: [authGuard]},
 	{path: 'settings/users/edit/:id', component: EditUserComponent, canActivate: [authGuard]},
 
 	{path: 'settings/roles', component: IndexRolComponent, canActivate: [authGuard]},
