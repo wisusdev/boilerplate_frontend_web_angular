09bf6214c40e4a92cf1e941159ff3dc9c1b7ccb1
Jesús Avelar <wisusdev@gmail.com>
2024-05-01 14:26:52 -0600

    Actualizar componente ShowUserComponent en show.component.html y show.component.ts

---
+++ src/app/resources/views/settings/users/show/show.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-show-user',
+	standalone: true,
+	imports: [
+		
+	],
+	templateUrl: './show.component.html'
+})
+export class ShowUserComponent {
+
+}
