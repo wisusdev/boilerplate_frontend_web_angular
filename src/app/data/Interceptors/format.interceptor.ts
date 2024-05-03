f0cab60bee2b38e67cef4979c9222c13a25da8dd
Jesús Avelar <wisusdev@gmail.com>
2024-05-03 10:49:46 -0600

    feat: Add format interceptor for HTTP requests

---
+++ src/app/data/Interceptors/format.interceptor.ts
@@ -0,0 +1,46 @@
+import {HttpInterceptorFn} from '@angular/common/http';
+
+export const formatInterceptor: HttpInterceptorFn = (req, next) => {
+	let cloneRequest = req.clone();
+
+	if(req.body !== null && typeof req.body === 'object'){
+
+		let requestBody = (req.body as any);
+		const requestType = requestBody.type;
+		delete requestBody.type;
+
+		if (req.method === 'POST') {
+			const body = {
+				data: {
+					type: requestType,
+					attributes: requestBody
+				}
+			}
+
+			cloneRequest = req.clone({
+				body: body
+			});
+		}
+
+		if (req.method === 'PATCH' || req.method === 'PUT') {
+			const requestId = requestBody.id;
+			delete requestBody.id;
+
+			const body = {
+				data: {
+					type: requestType,
+					id: requestId,
+					attributes: requestBody
+				}
+			}
+
+			cloneRequest = req.clone({
+				body: body
+			});
+		}
+
+		console.log(cloneRequest);
+	}
+
+	return next(cloneRequest);
+};
