4dea0f0ed59718fecde7839f2005582515308257
Jesús Avelar <wisusdev@gmail.com>
2024-05-03 10:51:13 -0600

    chore: Refactor login component code formatting and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgOptimizedImage } from "@angular/common";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgOptimizedImage} from "@angular/common";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from "./resources/views/shared/navbar/navbar.component";
-import { ToastComponent } from "./resources/components/toast/toast.component";
-import { ThemeComponent } from "./resources/components/theme/theme.component";
-import { HttpClient, HttpClientModule, provideHttpClient, withFetch, withInterceptors } from "@angular/common/http";
-import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
-import { TranslateHttpLoader } from "@ngx-translate/http-loader";
-import { ResetPasswordComponent } from './resources/views/auth/reset-password/reset-password.component';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { LoginComponent } from "./resources/views/auth/login/login.component";
-import { RegisterComponent } from "./resources/views/auth/register/register.component";
-import { ForgotPasswordComponent } from "./resources/views/auth/forgot-password/forgot-password.component";
-import { ProfileComponent } from "./resources/views/account/profile/profile.component";
-import { AccountMenuListComponent } from "./resources/views/account/account-menu-list/account-menu-list.component";
-import { ChangePasswordComponent } from "./resources/views/account/change-password/change-password.component";
-import { DeviceConnectedComponent } from "./resources/views/account/device-connected/device-connected.component";
-import { ConfirmationDialogComponent } from "./resources/components/confirmation-dialog/confirmation-dialog.component";
-import { authInterceptor } from './data/Interceptors/auth.interceptor';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from "./resources/views/shared/navbar/navbar.component";
+import {ToastComponent} from "./resources/components/toast/toast.component";
+import {ThemeComponent} from "./resources/components/theme/theme.component";
+import {HttpClient, HttpClientModule, provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {ResetPasswordComponent} from './resources/views/auth/reset-password/reset-password.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {LoginComponent} from "./resources/views/auth/login/login.component";
+import {RegisterComponent} from "./resources/views/auth/register/register.component";
+import {ForgotPasswordComponent} from "./resources/views/auth/forgot-password/forgot-password.component";
+import {ProfileComponent} from "./resources/views/account/profile/profile.component";
+import {AccountMenuListComponent} from "./resources/views/account/account-menu-list/account-menu-list.component";
+import {ChangePasswordComponent} from "./resources/views/account/change-password/change-password.component";
+import {DeviceConnectedComponent} from "./resources/views/account/device-connected/device-connected.component";
+import {ConfirmationDialogComponent} from "./resources/components/confirmation-dialog/confirmation-dialog.component";
+import {authInterceptor} from './data/Interceptors/auth.interceptor';
 import {SidebarComponent} from "./resources/views/shared/sidebar/sidebar.component";
+import {formatInterceptor} from "./data/Interceptors/format.interceptor";
 
 import {
 	NgbCollapse,
@@ -74,13 +75,12 @@ import {
 		provideHttpClient(
 			withFetch(),
 			withInterceptors([
-				authInterceptor
+				authInterceptor,
+				formatInterceptor
 			])
 		)
 	],
-	exports: [
-
-	],
+	exports: [],
 	bootstrap: [AppComponent]
 })
 export class AppModule {
