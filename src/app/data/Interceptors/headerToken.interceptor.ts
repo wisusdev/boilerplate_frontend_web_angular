5a18fb5c1bcfa77ec16536d604c91041dd9ba650
Jesús Avelar <wisusdev@gmail.com>
2024-05-03 18:15:55 -0600

    feat: Add headerTokenInterceptor for HTTP requests

---
+++ src/app/data/Interceptors/headerToken.interceptor.ts
@@ -0,0 +1,33 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import {catchError} from "rxjs";
+import {inject} from "@angular/core";
+import {Router} from "@angular/router";
+import {app} from "../../config/App";
+
+export const headerTokenInterceptor: HttpInterceptorFn = (req, next) => {
+	const router = inject(Router);
+	let response = next(req);
+
+	const access_token: string | null = localStorage.getItem('access_token');
+
+	if(access_token) {
+		const authReq = req.clone({
+			setHeaders: {
+				Authorization: `Bearer ${access_token}`
+			}
+		});
+
+		response = next(authReq).pipe(
+			catchError((error) => {
+				if (error.status === 401) {
+					localStorage.clear();
+					router.navigate([app.redirectToLogin]).then();
+				}
+
+				throw error;
+			})
+		);
+	}
+
+	return response;
+};
