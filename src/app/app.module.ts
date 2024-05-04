733d8d7f80b23fcb60bd82b87bd985ff73f885c8
Jesús Avelar <wisusdev@gmail.com>
2024-05-03 22:25:42 -0600

    Remove unused components and update import statements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,22 @@ import {NgOptimizedImage} from "@angular/common";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {NavbarComponent} from "./resources/views/shared/navbar/navbar.component";
-import {ToastComponent} from "./resources/components/toast/toast.component";
-import {ThemeComponent} from "./resources/components/theme/theme.component";
 import {HttpClient, HttpClientModule, provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-import {ResetPasswordComponent} from './resources/views/auth/reset-password/reset-password.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {LoginComponent} from "./resources/views/auth/login/login.component";
-import {RegisterComponent} from "./resources/views/auth/register/register.component";
-import {ForgotPasswordComponent} from "./resources/views/auth/forgot-password/forgot-password.component";
-import {ProfileComponent} from "./resources/views/account/profile/profile.component";
-import {AccountMenuListComponent} from "./resources/views/account/account-menu-list/account-menu-list.component";
-import {ChangePasswordComponent} from "./resources/views/account/change-password/change-password.component";
-import {DeviceConnectedComponent} from "./resources/views/account/device-connected/device-connected.component";
-import {ConfirmationDialogComponent} from "./resources/components/confirmation-dialog/confirmation-dialog.component";
 import {headerTokenInterceptor} from './data/Interceptors/headerToken.interceptor';
-import {SidebarComponent} from "./resources/views/shared/sidebar/sidebar.component";
 import {formatRequestInterceptor} from "./data/Interceptors/formatRequest.interceptor";
+import {LoginComponent} from "./views/auth/login/login.component";
+import {RegisterComponent} from "./views/auth/register/register.component";
+import {ForgotPasswordComponent} from "./views/auth/forgot-password/forgot-password.component";
+import {ResetPasswordComponent} from "./views/auth/reset-password/reset-password.component";
+import {ProfileComponent} from "./views/account/profile/profile.component";
+import {ChangePasswordComponent} from "./views/account/change-password/change-password.component";
+import {DeviceConnectedComponent} from "./views/account/device-connected/device-connected.component";
+import {AccountMenuListComponent} from "./views/account/account-menu-list/account-menu-list.component";
+import {SidebarComponent} from "./views/layout/sidebar/sidebar.component";
+import {NavbarComponent} from "./views/layout/navbar/navbar.component";
 
 import {
 	NgbCollapse,
@@ -32,6 +29,9 @@ import {
 	NgbDropdownToggle,
 	NgbPaginationModule
 } from "@ng-bootstrap/ng-bootstrap";
+import {ConfirmationDialogComponent} from "./views/shared/confirmation-dialog/confirmation-dialog.component";
+import {ToastComponent} from "./views/shared/toast/toast.component";
+import {ThemeComponent} from "./views/shared/theme/theme.component";
 
 @NgModule({
 	declarations: [
