9ef5c50985ddeb43f2bfeb79d1a53006993c19f2
Jesús Avelar <wisusdev@gmail.com>
2024-05-03 22:25:03 -0600

    chore: Update routing module imports and remove unused components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccountRoutingModule } from "./resources/views/account/account-routing.module";
-import { AuthRoutingModule } from "./resources/views/auth/auth-routing.module";
-import { SettingsRoutingModule } from "./resources/views/settings/settings-routing.module";
-import { HomeComponent } from './resources/views/home/home.component';
 import { authGuard } from './data/Guards/Auth.guard';
+import {HomeComponent} from "./views/home/home.component";
+import {AccountRoutingModule} from "./views/account/account-routing.module";
+import {AuthRoutingModule} from "./views/auth/auth-routing.module";
+import {SettingsRoutingModule} from "./views/settings/settings-routing.module";
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'auth/login', pathMatch: 'full' },
