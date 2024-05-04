10b061855d123730395a577672caf606d771c599
Jesús Avelar <wisusdev@gmail.com>
2024-05-03 18:20:28 -0600

    feat: Update forgot password component with translation and error handling

--- src/app/resources/views/auth/forgot-password/forgot-password.component.ts
+++ src/app/resources/views/auth/forgot-password/forgot-password.component.ts
@@ -2,8 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../auth.service';
 import {Handle} from '../../../../data/Exceptions/Handle';
-import {ErrorMessages} from '../../../../data/Interfaces/Errors.interface';
+import {ErrorMessagesInterface} from '../../../../data/Interfaces/Errors.interface';
 import {catchError, of, tap} from "rxjs";
+import {app} from "../../../../config/App";
+import {TranslateService} from "@ngx-translate/core";
+import {ToastService} from "../../../../data/Services/Toast.service";
 
 @Component({
 	selector: 'app-forgot-password',
@@ -11,12 +14,18 @@ import {catchError, of, tap} from "rxjs";
 })
 export class ForgotPasswordComponent implements OnInit {
 
-	constructor(private formBuilder: FormBuilder, private authService: AuthService, private handleMessage: Handle) {
+	constructor(
+		private formBuilder: FormBuilder,
+		private authService: AuthService,
+		private handleMessage: Handle,
+		private translate: TranslateService,
+		private toast: ToastService
+	) {
 	}
 
 	formForgotPassword!: FormGroup;
 
-	errorMessages: ErrorMessages = {
+	errorMessages: ErrorMessagesInterface = {
 		email: ''
 	};
 
@@ -28,7 +37,8 @@ export class ForgotPasswordComponent implements OnInit {
 
 	ngOnInit() {
 		this.formForgotPassword = this.formBuilder.group({
-			email: ['', Validators.required]
+			type: 'users',
+			email: ['', [Validators.required, Validators.email]]
 		});
 
 		if (this.formForgotPassword !== undefined) {
@@ -37,29 +47,21 @@ export class ForgotPasswordComponent implements OnInit {
 	}
 
 	onSubmit() {
-		let userForgotPasswordData = this.formatFormUser(this.formForgotPassword.value);
 		this.resetErrorMessages();
-		this.authService.forgotPassword(userForgotPasswordData).pipe(
+		this.authService.forgotPassword(this.formForgotPassword.value).pipe(
 			tap(response => {
-				this.handleMessage.handleResponse('Successfully sent reset password email', this.formForgotPassword, '/auth/login');
+				this.handleMessage.handleResponse(this.translate.instant('message.resetPasswordEmailSent'), this.formForgotPassword, app.redirectToLogin);
 			}),
 			catchError(error => {
 				if (typeof error === 'object') {
 					for (let key in error) {
-						let keyName = error[key]['title'].split('.')[1];
+						let keyName = error[key]['title'].split('.')[2];
 						this.errorMessages[keyName] = error[key]['detail'];
 					}
 				}
+				this.toast.danger(this.translate.instant('message.failingSendingEmail'));
 				return of(null);
 			})
 		).subscribe();
-	}
-
-	formatFormUser(formUser: any) {
-		return {
-			"data": {
-				"email": formUser.email,
-			}
-		};
 	}
 }
