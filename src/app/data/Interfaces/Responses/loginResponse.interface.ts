fdac8cf5ce7cab49c5b9b9daf190cb7f743adfdf
Jesús Avelar <wisusdev@gmail.com>
2024-05-03 18:16:57 -0600

    feat: Add LoginResponseInterface for login API response

---
+++ src/app/data/Interfaces/Responses/loginResponse.interface.ts
@@ -0,0 +1,25 @@
+export interface LoginResponseInterface {
+    data: {
+        type: string;
+        id: string;
+        attributes: {
+            user: {
+                first_name: string;
+                last_name: string;
+                username: string;
+                email: string;
+                avatar: string | null;
+                language: string;
+            };
+        };
+        relationships: {
+            roles: string[];
+            permissions: any[];
+            access: {
+                token: string;
+                token_type: string;
+                expires_at: string;
+            };
+        };
+    };
+}
