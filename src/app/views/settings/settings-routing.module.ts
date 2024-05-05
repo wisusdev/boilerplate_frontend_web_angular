5122c60339ba156ccaeb7c9c3662710b0f607077
Jesús Avelar <wisusdev@gmail.com>
2024-05-05 14:14:17 -0600

    chore: Update settings routing to include user and role permissions

--- src/app/views/settings/settings-routing.module.ts
+++ src/app/views/settings/settings-routing.module.ts
@@ -10,14 +10,14 @@ import { ShowUserComponent } from "./users/show/show.component";
 import {authGuard} from "../../data/Guards/Auth.guard";
 
 const router: Routes = [
-	{path: 'settings/users', component: IndexUserComponent, canActivate: [authGuard]},
-	{path: 'settings/users/create', component: CreateUserComponent, canActivate: [authGuard]},
-	{path: 'settings/users/show/:id', component: ShowUserComponent, canActivate: [authGuard]},
-	{path: 'settings/users/edit/:id', component: EditUserComponent, canActivate: [authGuard]},
+	{path: 'settings/users', component: IndexUserComponent, canActivate: [authGuard], data: {permissions: 'users.index'}},
+	{path: 'settings/users/create', component: CreateUserComponent, canActivate: [authGuard], data: {permissions: 'users.create'}},
+	{path: 'settings/users/show/:id', component: ShowUserComponent, canActivate: [authGuard], data: {permissions: 'users.show'}},
+	{path: 'settings/users/edit/:id', component: EditUserComponent, canActivate: [authGuard], data: {permissions: 'users.edit'}},
 
-	{path: 'settings/roles', component: IndexRolComponent, canActivate: [authGuard]},
-	{path: 'settings/roles/create', component: CreateRolComponent, canActivate: [authGuard]},
-	{path: 'settings/roles/edit/:id', component: EditRolComponent, canActivate: [authGuard]},
+	{path: 'settings/roles', component: IndexRolComponent, canActivate: [authGuard], data: {permissions: 'roles.index'}},
+	{path: 'settings/roles/create', component: CreateRolComponent, canActivate: [authGuard], data: {permissions: 'roles.create'}},
+	{path: 'settings/roles/edit/:id', component: EditRolComponent, canActivate: [authGuard], data: {permissions: 'roles.edit'}},
 ];
 
 @NgModule({
