02c4f0b45545fbdc79ae0d9b17d68807690b8e20
Jesús Avelar <wisusdev@gmail.com>
2024-05-07 11:07:37 -0600

    feat: Add StoreUserResponseInterface for retrieving user data

---
+++ src/app/data/Interfaces/Responses/storeUserResponse.interface.ts
@@ -0,0 +1,18 @@
+export interface StoreUserResponseInterface {
+	data: {
+		type: string;
+		id: string;
+		attributes: {
+			username: string;
+			first_name: string;
+			last_name: string;
+			email: string;
+			email_verified_at: string | null;
+			created_at: string;
+			updated_at: string;
+		},
+		relationships: {
+			roles: string[];
+		}
+	}
+}
