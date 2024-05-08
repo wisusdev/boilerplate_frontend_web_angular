7d68872397ba431cff5e85f512c3ff5cb34ce586
Jesús Avelar <wisusdev@gmail.com>
2024-05-07 23:54:32 -0600

    chore: Update app configuration with language setting

--- src/app/config/App.ts
+++ src/app/config/App.ts
@@ -2,6 +2,12 @@ export const app = {
 	/* Application Name */
 	name: 'Angular App',
 
+	/* Application Version */
+	version: '1.0.0',
+
+	/* Lang */
+	lang: 'en',
+
 	/* Application URl */
 	app_url: 'http://localhost:4200',
 
