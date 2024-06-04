5a8e6588d4f2fa4012af124ccb7c0109fd745dd2
Jesús Avelar <wisusdev@gmail.com>
2024-06-03 23:42:18 -0600

    feat: Add index subscription component for displaying subscriptions

---
+++ src/app/views/services/subscriptions/index/indexSubscription.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-index-subscription',
+	standalone: true,
+	imports: [],
+	templateUrl: './indexSubscription.component.html',
+})
+export class IndexSubscriptionComponent {
+
+}
