2f9e469ed3990095098ca69dc8ecc042601d237b
Jesús Avelar <wisusdev@gmail.com>
2024-06-29 15:16:23 -0600

    feat: Add Auth and RoutesException imports to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import { Auth } from './data/Providers/Auth';
+import {Auth} from './data/Providers/Auth';
+import {routesException} from "./data/Vendor/RoutesException";
 
 @Component({
 	selector: 'app-root',
@@ -8,14 +9,19 @@ import { Auth } from './data/Providers/Auth';
 })
 export class AppComponent implements OnInit {
 
+	constructor(
+		private authUser: Auth
+	) {
+	}
+
 	public loggedIn: boolean = false;
 	title = 'angular-app';
-
-	constructor(private authUser: Auth) { }
 
 	ngOnInit() {
 		this.authUser.status().pipe().subscribe((status: boolean) => {
 			this.loggedIn = status;
 		});
 	}
+
+	protected readonly routesException = routesException;
 }
