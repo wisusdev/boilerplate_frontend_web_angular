609f02d5a5bf2428b9df65bb5acf69865af33412
Jesús Avelar <wisusdev@gmail.com>
2024-06-28 23:31:24 -0600

    feat: Add WelcomeComponent and update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import {AccountRoutingModule} from "./views/account/account-routing.module";
 import {AuthRoutingModule} from "./views/auth/auth-routing.module";
 import {SettingsRoutingModule} from "./views/settings/settings-routing.module";
 import {ServicesRoutingModule} from "./views/services/services-routing.module";
+import {WelcomeComponent} from './views/welcome/welcome.component';
 
 const routes: Routes = [
-	{ path: '', redirectTo: 'auth/login', pathMatch: 'full' },
-	{ path: 'home', component: HomeComponent, canActivate: [authGuard] }
+	{ path: '', component: WelcomeComponent },
+	{ path: 'home', component: HomeComponent, canActivate: [authGuard] },
+	{ path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
