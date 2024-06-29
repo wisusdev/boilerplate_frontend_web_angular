5953799dc46a0c0f090c62bdcf41e8cb6da8445a
Jesús Avelar <wisusdev@gmail.com>
2024-06-29 15:16:02 -0600

    feat: Update app-routing.module.ts to use correct component paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { authGuard } from './data/Guards/Auth.guard';
-import {HomeComponent} from "./views/home/home.component";
-import {AccountRoutingModule} from "./views/account/account-routing.module";
-import {AuthRoutingModule} from "./views/auth/auth-routing.module";
-import {SettingsRoutingModule} from "./views/settings/settings-routing.module";
-import {ServicesRoutingModule} from "./views/services/services-routing.module";
-import {WelcomeComponent} from './views/welcome/welcome.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {authGuard} from './data/Guards/Auth.guard';
+import {HomeComponent} from "./views/admin/home/home.component";
+import {WelcomeComponent} from "./views/public/welcome/welcome.component";
+import {AccountRoutingModule} from "./views/admin/account/account-routing.module";
+import {SettingsRoutingModule} from "./views/admin/settings/settings-routing.module";
+import {AuthRoutingModule} from "./views/public/auth/auth-routing.module";
+import {ServicesRoutingModule} from "./views/admin/services/services-routing.module";
 
 const routes: Routes = [
-	{ path: '', component: WelcomeComponent },
-	{ path: 'home', component: HomeComponent, canActivate: [authGuard] },
-	{ path: '**', redirectTo: '', pathMatch: 'full' }
+	{path: '', component: WelcomeComponent},
+	{path: 'home', component: HomeComponent, canActivate: [authGuard]},
+	{path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
 @NgModule({
