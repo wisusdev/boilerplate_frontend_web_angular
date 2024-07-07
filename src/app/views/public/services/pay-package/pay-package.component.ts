0e3ed59074571e4df512369685dac66fd60bafdb
Jesús Avelar <wisusdev@gmail.com>
2024-07-06 23:09:28 -0600

    feat: Add pay-package component for paying services

---
+++ src/app/views/public/services/pay-package/pay-package.component.ts
@@ -0,0 +1,128 @@
+import {Component, OnInit} from '@angular/core';
+import {Auth} from "@data/Providers/Auth";
+import {LoginComponent} from "../../auth/login/login.component";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {ServicesService} from "@views/admin/services/services.service";
+import {ActivatedRoute} from "@angular/router";
+import {catchError, of, tap} from "rxjs";
+import {PackageData} from "@data/Interfaces/Responses/showPackageResponse.interface";
+import {CurrencyPipe} from "@angular/common";
+import {PublicService} from "@views/public/services/public.service";
+
+interface UserProfile {
+	id: string;
+	first_name: string;
+	last_name: string;
+	email: string;
+	avatar: string;
+}
+
+@Component({
+	selector: 'app-pay-service',
+	standalone: true,
+	imports: [
+		LoginComponent,
+		TranslateModule,
+		CurrencyPipe
+	],
+	templateUrl: './pay-package.component.html'
+})
+export class PayPackageComponent implements OnInit {
+	public loggedIn: boolean = false;
+	public serviceId: string = '';
+	private userId: string = '';
+
+	public package: PackageData = {
+		type: '',
+		id: '',
+		attributes: {
+			name: '',
+			description: '',
+			max_users: 0,
+			interval: '',
+			interval_count: 0,
+			price: '',
+			trial_days: 0,
+			active: 0,
+			created_by: '',
+			created_at: '',
+			updated_at: ''
+		}
+	};
+
+	public userProfile: UserProfile = {
+		id: '',
+		first_name: '',
+		last_name: '',
+		email: '',
+		avatar: '',
+	}
+
+	constructor(
+		private authUser: Auth,
+		protected translate: TranslateService,
+		private services: PublicService,
+		private route: ActivatedRoute,
+	) {
+	}
+
+	ngOnInit() {
+		this.authUser.status().pipe().subscribe((status: boolean) => {
+			this.loggedIn = status;
+		});
+
+		this.route.params.subscribe(params => {
+			this.serviceId = params['id'];
+		});
+
+		this.getUserProfile();
+		this.getUserId();
+		this.getPublicService();
+	}
+
+	getPublicService(id: string = this.serviceId){
+		this.services.publicShowPackage(id).pipe(
+			tap((response) => {
+				this.package = response.data;
+			}),
+			catchError((error) => {
+				console.error(error);
+				return of(null);
+			})
+		).subscribe()
+	}
+
+	storeSubscriptions(payment_method: string){
+		let data = {
+			type: 'subscriptions',
+			user_id: this.userId,
+			package_id: this.package.id,
+			created_by: this.userId,
+			payment_method: payment_method,
+		};
+
+		this.services.publicStoreSubscription(data).pipe(
+			tap((response) => {
+				console.log(response);
+			}),
+			catchError((error) => {
+				console.error(error);
+				return of(null);
+			})
+		).subscribe();
+	}
+
+	getUserProfile(){
+		let userData = localStorage.getItem('user');
+		if (userData) {
+			this.userProfile = JSON.parse(userData);
+		}
+	}
+
+	getUserId(){
+		let userId = localStorage.getItem('user_key');
+		if (userId) {
+			this.userId = userId;
+		}
+	}
+}
