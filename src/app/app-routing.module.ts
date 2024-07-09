f4fa9e5d418050f0204ea20ef069144d5d604ba4
Jesús Avelar <wisusdev@gmail.com>
2024-07-09 16:19:30 -0600

    feat: Update routing paths for public packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {authGuard} from './data/Guards/Auth.guard';
-import {HomeComponent} from "./views/admin/home/home.component";
-import {WelcomeComponent} from "./views/public/welcome/welcome.component";
-import {AccountRoutingModule} from "./views/admin/account/account-routing.module";
-import {SettingsRoutingModule} from "./views/admin/settings/settings-routing.module";
-import {AuthRoutingModule} from "./views/public/auth/auth-routing.module";
-import {ServicesRoutingModule} from "./views/admin/services/services-routing.module";
-import { PublicPackagesComponent } from './views/public/services/package/public-packages.component';
-import {PayPackageComponent} from "./views/public/services/pay-package/pay-package.component";
+import {authGuard} from '@data/Guards/Auth.guard';
+import {HomeComponent} from "@views/admin/home/home.component";
+import {WelcomeComponent} from "@views/public/welcome/welcome.component";
+import {AccountRoutingModule} from "@views/admin/account/account-routing.module";
+import {SettingsRoutingModule} from "@views/admin/settings/settings-routing.module";
+import {AuthRoutingModule} from "@views/public/auth/auth-routing.module";
+import {ServicesRoutingModule} from "@views/admin/services/services-routing.module";
+import { PublicPackagesComponent } from '@views/public/services/package/public-packages.component';
+import {PayPackageComponent} from "@views/public/services/pay-package/pay-package.component";
 
 const routes: Routes = [
 	{path: '', component: WelcomeComponent},
 	{path: 'home', component: HomeComponent, canActivate: [authGuard]},
 	{path: 'plans', component: PublicPackagesComponent},
-	{path: 'plans/:id', component: PayPackageComponent},
+	{path: 'plans/:id', component: PayPackageComponent, canActivate: [authGuard]},
+	{path: 'payment-success/:id', component: PayPackageComponent, canActivate: [authGuard]},
 	{path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
