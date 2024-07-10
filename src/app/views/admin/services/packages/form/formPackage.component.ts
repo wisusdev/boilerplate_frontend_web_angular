76ab206aae343f82b9582cd4a74331ed3df6bf4e
Jesús Avelar <wisusdev@gmail.com>
2024-07-09 23:11:05 -0600

    feat: Update FormPackageComponent to populate 'limits' field with max_users value

--- src/app/views/admin/services/packages/form/formPackage.component.ts
+++ src/app/views/admin/services/packages/form/formPackage.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, Input, InputSignal, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
@@ -63,8 +63,28 @@ export class FormPackageComponent implements OnInit {
 	getPackage(packageId: string) {
 		this.services.showPackage(packageId).pipe(
 			tap((response) => {
-				console.log(response);
-				this.formPackage.patchValue(response.data.attributes);
+
+				const limits = response.data.attributes.limits;
+				let max_users: number = 0;
+				let interval: number = 0;
+
+				if(limits !== null) {
+					const limitsObj = JSON.parse(limits);
+					max_users = limitsObj.max_users;
+					interval = limitsObj.interval;
+				}
+
+				this.formPackage.patchValue({
+					name: response.data.attributes.name,
+					max_users: max_users,
+					interval: response.data.attributes.interval,
+					interval_count: response.data.attributes.interval_count,
+					price: response.data.attributes.price,
+					trial_days: response.data.attributes.trial_days,
+					active: response.data.attributes.active,
+					description: response.data.attributes.description,
+					created_by: response.data.attributes.created_by,
+				});
 			}),
 			catchError((error) => {
 				this.toast.danger(this.translate.instant('errorAsOccurred'));
@@ -74,10 +94,19 @@ export class FormPackageComponent implements OnInit {
 	}
 
 	public submit() {
+		// Obteniendo el valor de max_users
+		const maxUsersValue = this.formPackage.value.max_users;
+		// Eliminando max_users del objeto
+		const {max_users, ...result} = this.formPackage.value;
+		// Crear el objeto limits con el valor de max_users
+		const limits = JSON.stringify({max_users: maxUsersValue});
+		// Agregar limits al objeto result
+		const newData = {...result, limits: limits};
+
 		const actionType = this.typeAction === 'create' ? 'create' : 'edit';
 		this.activeModal.close({
 			type: actionType,
-			data: this.formPackage.value
+			data: newData
 		});
 	}
 
