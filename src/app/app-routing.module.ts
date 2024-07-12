27f5adb28cc05b8c695bd7309c5caa9b6c1bcb48
Jesús Avelar <wisusdev@gmail.com>
2024-07-11 22:48:58 -0600

    feat: Update routing paths for public packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,15 @@ import {AuthRoutingModule} from "@views/public/auth/auth-routing.module";
 import {ServicesRoutingModule} from "@views/admin/services/services-routing.module";
 import { PublicPackagesComponent } from '@views/public/services/package/public-packages.component';
 import {PayPackageComponent} from "@views/public/services/pay-package/pay-package.component";
+import {PaymentStatusComponent} from "@views/public/services/payment-status/payment-status.component";
 
 const routes: Routes = [
 	{path: '', component: WelcomeComponent},
 	{path: 'home', component: HomeComponent, canActivate: [authGuard]},
 	{path: 'plans', component: PublicPackagesComponent},
 	{path: 'plans/:id', component: PayPackageComponent, canActivate: [authGuard]},
-	{path: 'payment-success/:id', component: PayPackageComponent, canActivate: [authGuard]},
+	{path: 'payment-success/:id', component: PaymentStatusComponent, canActivate: [authGuard]},
+	{path: 'payment-cancelled', component: PaymentStatusComponent, canActivate: [authGuard]},
 	{path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
