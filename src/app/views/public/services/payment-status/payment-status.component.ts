8386b5275af3cfeb23c37be767647afa63b6b1b3
Jesús Avelar <wisusdev@gmail.com>
2024-07-12 23:28:54 -0600

    feat: Update PaymentStatusComponent to handle payment status and update subscription status

--- src/app/views/public/services/payment-status/payment-status.component.ts
+++ src/app/views/public/services/payment-status/payment-status.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {PublicService} from "@views/public/services/public.service";
+import {catchError, of, tap} from "rxjs";
+import {ToastService} from "@data/Services/Toast.service";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
 	selector: 'app-payment-status',
@@ -10,14 +14,20 @@ import {ActivatedRoute} from "@angular/router";
 export class PaymentStatusComponent implements OnInit {
 
 	statusPayment: string = 'success';
+	subscriptionId: string = '';
+	packageId: string = '';
 
 	constructor(
 		private route: ActivatedRoute,
+		private publicService: PublicService,
+		private toast: ToastService,
+		private translate: TranslateService
 	) {}
 
 	ngOnInit() {
 		this.route.url.subscribe(urlSegment => {
 			const statusSegment = urlSegment[0].path;
+			this.packageId = urlSegment[1].path;
 			if(statusSegment === 'payment-success') {
 				this.statusPayment = 'success';
 			} else {
@@ -28,17 +38,29 @@ export class PaymentStatusComponent implements OnInit {
 		this.route.queryParams.subscribe(params => {
 			const subscriptionId = params['subscription_id'];
 			if (subscriptionId) {
-				const data = {
-					subscriptionId: subscriptionId,
-					status: this.statusPayment
-				};
-				this.updateStatusPayment(data);
+				this.subscriptionId = subscriptionId;
 			}
 		});
+
+		this.updateStatusPayment();
 	}
 
-	updateStatusPayment(data: object) {
+	updateStatusPayment() {
+		const data = {
+			type: 'subscriptions',
+			id: this.packageId,
+			subscription_id: this.subscriptionId,
+		};
 
+		this.publicService.publicUpdateSubscription(this.packageId, data).pipe(
+			tap(() => {
+				this.toast.success(this.translate.instant('recordCreated'));
+			}),
+			catchError(() => {
+				this.toast.danger(this.translate.instant('errorAsOccurred'));
+				return of(null);
+			})
+		).subscribe();
 	}
 
 }
