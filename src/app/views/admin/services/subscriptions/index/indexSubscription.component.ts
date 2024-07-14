05405d245ace895a4437299f6b6a5a768a5c7c7b
Jesús Avelar <wisusdev@gmail.com>
2024-07-13 23:32:59 -0600

    feat: Update subscription table to display payment status badge

--- src/app/views/admin/services/subscriptions/index/indexSubscription.component.ts
+++ src/app/views/admin/services/subscriptions/index/indexSubscription.component.ts
@@ -3,9 +3,13 @@ import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {ServicesService} from "../../services.service";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {catchError, of, tap} from "rxjs";
-import {DatePipe, NgForOf} from "@angular/common";
+import {DatePipe, NgClass, NgForOf} from "@angular/common";
 import {SubscriptionData} from "@data/Interfaces/Responses/indexSubscriptionsResponse.interface";
 import {ToastService} from "@data/Services/Toast.service";
+
+interface StatusClass {
+	[key: string]: string;
+}
 
 @Component({
 	selector: 'app-index-subscription',
@@ -13,7 +17,8 @@ import {ToastService} from "@data/Services/Toast.service";
 	imports: [
 		TranslateModule,
 		NgForOf,
-		DatePipe
+		DatePipe,
+		NgClass
 	],
 	templateUrl: './indexSubscription.component.html',
 })
@@ -24,13 +29,21 @@ export class IndexSubscriptionComponent implements OnInit {
 		protected translate: TranslateService,
 		private services: ServicesService,
 		private modalService: NgbModal,
-	) {}
+	) {
+	}
 
 	lastPage: number = 0;
 	totalPages: number = 0;
 	pageNumber: number = 1;
 	pages: number[] = [];
 	subscriptions: SubscriptionData[] = [];
+
+	status: StatusClass = {
+		'approved': 'success',
+		'waiting': 'warning',
+		'declined': 'danger',
+		'cancel': 'danger'
+	};
 
 	ngOnInit() {
 		this.getSubscriptions();
