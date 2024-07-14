903fe3ebaccfd55af2a0e7456ef793b4d8998ecc
Jesús Avelar <wisusdev@gmail.com>
2024-07-13 23:31:57 -0600

    feat: Agregar componente AccountSubscriptions para manejar las suscripciones de la cuenta

--- src/app/views/admin/account/account-routing.module.ts
+++ src/app/views/admin/account/account-routing.module.ts
@@ -1,16 +1,18 @@
-import {ProfileComponent} from "./profile/profile.component";
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
-import {ChangePasswordComponent} from "./change-password/change-password.component";
-import {DeviceConnectedComponent} from "./device-connected/device-connected.component";
-import {DeleteAccountComponent} from "./delete-account/delete-account.component";
-import {authGuard} from "../../../data/Guards/Auth.guard";
+import {authGuard} from "@data/Guards/Auth.guard";
+import {AccountSubscriptionsComponent} from "@views/admin/account/account-subscriptions/account-subscriptions.component";
+import {ProfileComponent} from "@views/admin/account/profile/profile.component";
+import {ChangePasswordComponent} from "@views/admin/account/change-password/change-password.component";
+import {DeviceConnectedComponent} from "@views/admin/account/device-connected/device-connected.component";
+import {DeleteAccountComponent} from "@views/admin/account/delete-account/delete-account.component";
 
 const routes: Routes = [
 	{path: 'account/profile', component: ProfileComponent, canActivate: [authGuard]},
 	{path: 'account/change-password', component: ChangePasswordComponent, canActivate: [authGuard]},
 	{path: 'account/device-connected', component: DeviceConnectedComponent, canActivate: [authGuard]},
-	{path: 'account/delete-account', component: DeleteAccountComponent, canActivate: [authGuard]}
+	{path: 'account/delete-account', component: DeleteAccountComponent, canActivate: [authGuard]},
+	{path: 'account/subscriptions', component: AccountSubscriptionsComponent, canActivate: [authGuard]}
 ];
 
 @NgModule({
