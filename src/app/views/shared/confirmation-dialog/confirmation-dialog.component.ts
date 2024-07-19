3f3d3e488883873a1b77cc811d7466d9e9b6509a
Jesús Avelar <wisusdev@gmail.com>
2024-07-18 23:15:26 -0600

    feat: Agregar soporte para cancelar suscripciones en el diálogo de confirmación

--- src/app/views/shared/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/views/shared/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,28 +1,60 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {TranslateService} from "@ngx-translate/core";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
 	selector: 'app-confirmation-dialog',
 	standalone: true,
+	imports: [
+		NgIf,
+		ReactiveFormsModule,
+	],
 	templateUrl: './confirmation-dialog.component.html',
 })
-export class ConfirmationDialogComponent {
-	constructor(
-		public activeModal: NgbActiveModal,
-		private translate: TranslateService,
-	) { }
-
+export class ConfirmationDialogComponent implements OnInit{
 	@Input() title: string = this.translate.instant('logout');
 	@Input() message: string = this.translate.instant('confirmAction');
 	@Input() confirmText: string = this.translate.instant('yesConfirm');
 	@Input() cancelText: string = this.translate.instant('noCancel');
+	@Input() reason: boolean = false;
+
+	data: any = {
+		status: false
+	};
+
+	constructor(
+		public activeModal: NgbActiveModal,
+		private formBuilder: FormBuilder,
+		protected translate: TranslateService,
+	) { }
+
+	formModal!: FormGroup;
+
+	ngOnInit() {
+		if (this.reason) {
+			this.formModal = this.formBuilder.group({
+				reasonTextarea: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(128)]]
+			});
+
+			if (this.formModal !== undefined) {
+				this.formModal.patchValue(this.formModal);
+			}
+		}
+	}
 
 	public confirm() {
-		this.activeModal.close(true);
+		this.data['status'] = true;
+
+		if (this.reason) {
+			this.data['reason'] = this.formModal.value.reasonTextarea;
+		}
+
+		this.activeModal.close(this.data);
 	}
 
 	public cancel() {
-		this.activeModal.dismiss(false);
+		this.activeModal.dismiss(this.data);
 	}
 }
