09be2a829e138d169b21de3880fc5525ff7b2b03
Jesús Avelar <wisusdev@gmail.com>
2024-07-20 18:36:46 -0600

    feat: Add showSubscription component

---
+++ src/app/views/admin/services/subscriptions/show/showSubscription.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-show',
+	standalone: true,
+	imports: [],
+	templateUrl: './showSubscription.component.html',
+})
+export class ShowSubscriptionComponent {
+
+}
