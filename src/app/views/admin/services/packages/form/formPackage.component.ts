cb91ca5ea70ea608036e6efc75d5077d26f78e3d
Jesús Avelar <wisusdev@gmail.com>
2024-07-20 18:36:35 -0600

    chore: Update package description form field height

--- src/app/views/admin/services/packages/form/formPackage.component.ts
+++ src/app/views/admin/services/packages/form/formPackage.component.ts
@@ -51,7 +51,7 @@ export class FormPackageComponent implements OnInit {
 			price: ['', Validators.required],
 			trial_days: [0, Validators.required],
 			active: ['', Validators.required],
-			description: ['', [Validators.required, Validators.min(10)]],
+			description: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(127)]],
 			created_by: Auth.userId(),
 		});
 
