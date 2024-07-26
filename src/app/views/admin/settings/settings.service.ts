975b4e8f0eb6e9cef72d33c934417cb113a38533
Jesús Avelar <wisusdev@gmail.com>
2024-07-26 14:40:24 -0600

    chore: Update SettingsService to use environment variable for API URL

--- src/app/views/admin/settings/settings.service.ts
+++ src/app/views/admin/settings/settings.service.ts
@@ -1,7 +1,6 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {catchError, Observable} from 'rxjs';
-import {Api} from 'src/app/config/Api';
 import {Handle} from 'src/app/data/Exceptions/Handle';
 import {IndexRoleResponseInterface} from "@data/Interfaces/Responses/indexRoleResponseInterface";
 import {StoreRoleRequestInterface} from "@data/Interfaces/Requests/storeRoleRequest.interface";
@@ -12,13 +11,14 @@ import {IndexUserResponseInterface, UserData} from "@data/Interfaces/Responses/i
 import {StoreUserRequestInterface} from "@data/Interfaces/Requests/storeUserRequest.interface";
 import {StoreUserResponseInterface} from "@data/Interfaces/Responses/storeUserResponse.interface";
 import {ShowUserResponseInterface} from "@data/Interfaces/Responses/showUserResponse.interface";
+import {environment} from "@env/environment.development";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class SettingsService {
 
-	private _apiUri: string = Api.api_url_v1;
+	private _apiUri: string = environment.api_url_v1;
 	private _apiUriRoles: string = this._apiUri + '/roles';
 	private _apiUriPermissions: string = this._apiUri + '/permissions';
 	private _apiUriUsers: string = this._apiUri + '/users';
@@ -31,67 +31,67 @@ export class SettingsService {
 
 	indexRoles(): Observable<IndexRoleResponseInterface> {
 		return this.httpClient.get<IndexRoleResponseInterface>(`${this._apiUriRoles}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	storeRole(data: StoreRoleRequestInterface): Observable<UpdateStoreRoleResponseInterface> {
 		return this.httpClient.post<UpdateStoreRoleResponseInterface>(`${this._apiUriRoles}`, data, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	showRole(id: string): Observable<ShowRoleResponseInterface> {
 		return this.httpClient.get<ShowRoleResponseInterface>(`${this._apiUriRoles}/${id}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	updateRole(id: string, data: StoreRoleRequestInterface): Observable<UpdateStoreRoleResponseInterface> {
 		return this.httpClient.patch<UpdateStoreRoleResponseInterface>(`${this._apiUriRoles}/${id}`, data, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	destroyRole(id: string): any {
 		return this.httpClient.delete(`${this._apiUriRoles}/${id}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	indexPermissions(): Observable<IndexPermissionsInterface> {
 		return this.httpClient.get<IndexPermissionsInterface>(`${this._apiUriPermissions}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	indexUsers(): Observable<IndexUserResponseInterface> {
 		return this.httpClient.get<IndexUserResponseInterface>(`${this._apiUriUsers}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	storeUser(data: StoreUserRequestInterface): Observable<StoreUserResponseInterface> {
 		return this.httpClient.post<StoreUserResponseInterface>(`${this._apiUriUsers}`, data, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	showUser(id: string): Observable<ShowUserResponseInterface> {
 		return this.httpClient.get<ShowUserResponseInterface>(`${this._apiUriUsers}/${id}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	updateUser(id: string, data: UserData): Observable<UserData> {
 		return this.httpClient.patch<UserData>(`${this._apiUriUsers}/${id}`, data, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	destroyUser(id: string): any {
 		return this.httpClient.delete(`${this._apiUriUsers}/${id}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 }
