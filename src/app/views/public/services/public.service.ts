31eee837b27c6827b4bd298e43b32e3f9e469936
Jesús Avelar <wisusdev@gmail.com>
2024-07-26 14:43:41 -0600

    chore: Update public.service.ts to use environment variable for API URL

--- src/app/views/public/services/public.service.ts
+++ src/app/views/public/services/public.service.ts
@@ -1,16 +1,16 @@
 import {Injectable} from '@angular/core';
 import {catchError, Observable} from "rxjs";
 import {IndexPackageRequestInterface} from "@data/Interfaces/Requests/indexPackageRequest.interface";
-import {Api} from "@config/Api";
 import {HttpClient} from "@angular/common/http";
 import {Handle} from "@data/Exceptions/Handle";
 import {ShowPackageResponseInterface} from "@data/Interfaces/Responses/showPackageResponse.interface";
+import {environment} from "@env/environment";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class PublicService {
-	private _apiUri: string = Api.api_url_v1;
+	private _apiUri: string = environment.api_url_v1;
 
 	constructor(
 		private httpClient: HttpClient,
@@ -19,25 +19,25 @@ export class PublicService {
 
 	publicIndexPackage(): Observable<IndexPackageRequestInterface> {
 		return this.httpClient.get<IndexPackageRequestInterface>(`${this._apiUri}/public/packages`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	publicShowPackage(id: string): Observable<ShowPackageResponseInterface> {
 		return this.httpClient.get<ShowPackageResponseInterface>(`${this._apiUri}/public/packages/${id}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	publicStoreSubscription(data: any): Observable<any> {
 		return this.httpClient.post<any>(`${this._apiUri}/public/subscriptions`, data, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	publicUpdateSubscription(paymentTransactionId: string, data: object): Observable<any> {
 		return this.httpClient.patch<any>(`${this._apiUri}/public/subscriptions/validate/${paymentTransactionId}`, data, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 }
