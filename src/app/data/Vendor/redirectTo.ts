452a47e0f81e2d99a3dce1459d059a92fe4f7686
Jesús Avelar <wisusdev@gmail.com>
2024-07-26 14:39:49 -0600

    chore: Update redirectTo.ts to use environment variable for redirect URL

--- src/app/data/Vendor/redirectTo.ts
+++ src/app/data/Vendor/redirectTo.ts
@@ -1,10 +1,10 @@
 import { Router } from '@angular/router';
 import { ToastService } from '../Services/Toast.service';
 import { TranslateService } from '@ngx-translate/core';
-import { app } from '../../config/App';
+import {environment} from "@env/environment";
 
 export const redirectToHomeWithMessage = (router: Router, toast: ToastService, translate: TranslateService) => {
-	router.navigate([app.redirectToHome]).then(() => {
+	router.navigate([environment.redirectToHome]).then(() => {
 		toast.show({
 			message: translate.instant('permissionNotGranted'),
 			className: 'bg-warning text-light'
