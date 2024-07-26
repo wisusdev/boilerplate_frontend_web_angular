93861bceff5107e7e7ef7df4fa9dee6bb0b9966e
Jesús Avelar <wisusdev@gmail.com>
2024-07-26 14:40:17 -0600

    chore: Update ServicesService to use environment variable for API URL

--- src/app/views/admin/services/services.service.ts
+++ src/app/views/admin/services/services.service.ts
@@ -1,19 +1,19 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {catchError, Observable} from "rxjs";
-import {Api} from "@config/Api";
 import {Handle} from "@data/Exceptions/Handle";
 import {IndexPackageRequestInterface} from "@data/Interfaces/Requests/indexPackageRequest.interface";
 import {StoreUpdatePackageResponseInterface} from "@data/Interfaces/Responses/storeUpdatePackageResponde.interface";
 import {StoreUpdatePackageRequestInterface} from "@data/Interfaces/Requests/storeUpdatePackageRequest.interface";
 import {ShowPackageResponseInterface} from "@data/Interfaces/Responses/showPackageResponse.interface";
 import {IndexSubscriptionsResponseInterface} from "@data/Interfaces/Responses/indexSubscriptionsResponse.interface";
+import {environment} from "@env/environment.development";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class ServicesService {
-	private _apiUri: string = Api.api_url_v1;
+	private _apiUri: string = environment.api_url_v1;
 	private _apiUriPackage: string = this._apiUri + '/packages';
 	private _apiUriSubscription: string = this._apiUri + '/subscriptions';
 
@@ -25,38 +25,38 @@ export class ServicesService {
 	/* PACKAGES */
 	indexPackage(): Observable<IndexPackageRequestInterface> {
 		return this.httpClient.get<IndexPackageRequestInterface>(`${this._apiUriPackage}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	createPackage(data: StoreUpdatePackageRequestInterface): Observable<StoreUpdatePackageResponseInterface> {
 		return this.httpClient.post<StoreUpdatePackageResponseInterface>(`${this._apiUriPackage}`, data, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	showPackage(id: string): Observable<ShowPackageResponseInterface> {
 		return this.httpClient.get<ShowPackageResponseInterface>(`${this._apiUriPackage}/${id}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	updatePackage(id: string, data: StoreUpdatePackageRequestInterface): Observable<StoreUpdatePackageResponseInterface> {
 		return this.httpClient.put<StoreUpdatePackageResponseInterface>(`${this._apiUriPackage}/${id}`, data, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	deletePackage(id: string): Observable<any> {
 		return this.httpClient.delete(`${this._apiUriPackage}/${id}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 
 	/* SUBSCRIPTIONS */
 	indexSubscriptions(): Observable<IndexSubscriptionsResponseInterface> {
 		return this.httpClient.get<IndexSubscriptionsResponseInterface>(`${this._apiUriSubscription}`, {
-			headers: Api.headers,
+			headers: environment.headers,
 		}).pipe(catchError(this.handleMessage.errorHandle));
 	}
 }
