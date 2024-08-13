5d4617ff1f9144f138f77daf9abd2c7e1919f0b1
Jesús Avelar <wisusdev@gmail.com>
2024-08-12 22:41:28 -0600

    feat: Refactor file helper service for handling file selection

---
+++ src/app/data/Services/file-helper.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core';
+import {Handle} from '@data/Exceptions/Handle';
+import {environment} from "@env/environment";
+
+@Injectable({
+	providedIn: 'root'
+})
+export class FileHelperService {
+	allowedTypes = environment.allowImageTypes;
+
+	constructor(
+		private handleMessage: Handle
+	) {}
+
+	handleFileSelection(event: any, callback: (base64: string) => void) {
+		let selectImage = <File>event.target.files[0];
+
+		if (selectImage.size > environment.allowImageSize) {
+			this.handleMessage.handleError('File size is too large');
+			event.target.value = '';
+			return;
+		}
+
+		if (!this.allowedTypes.includes(selectImage.type)) {
+			this.handleMessage.handleError('Invalid file type');
+			event.target.value = '';
+			return;
+		}
+
+		const reader = new FileReader();
+
+		reader.onload = () => {
+			let base64String = reader.result as string;
+			callback(base64String);
+		};
+
+		reader.readAsDataURL(selectImage);
+	}
+}
