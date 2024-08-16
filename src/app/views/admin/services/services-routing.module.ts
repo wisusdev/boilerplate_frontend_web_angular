290a89c70529e3fefe37589329e72adf59571c8a
Jesús Avelar <wisusdev@gmail.com>
2024-08-15 22:19:35 -0600

    feat: Add "Invoices" page to services routing module

--- src/app/views/admin/services/services-routing.module.ts
+++ src/app/views/admin/services/services-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {IndexPackageComponent} from "./packages/index/indexPackage.component";
 import {IndexSubscriptionComponent} from "./subscriptions/index/indexSubscription.component";
 import {authGuard} from "@data/Guards/Auth.guard";
+import {IndexInvoiceComponent} from "@views/admin/services/invoices/index/indexInvoice.component";
 
 const routes: Routes = [
 	{path: 'services/packages', component: IndexPackageComponent, canActivate: [authGuard], data: {permissions: 'packages.index'}},
 	{path: 'services/subscriptions', component: IndexSubscriptionComponent, canActivate: [authGuard], data: {permissions: 'subscriptions.index'}},
+	{path: 'services/invoices', component: IndexInvoiceComponent, canActivate: [authGuard], data: {permissions: 'invoices.index'}},
 ];
 
 @NgModule({
