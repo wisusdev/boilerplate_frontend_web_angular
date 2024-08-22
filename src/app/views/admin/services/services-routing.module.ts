1c91c2fb6c9912ff88fa16fbd53258594a9d171f
Jesús Avelar <wisusdev@gmail.com>
2024-08-21 22:28:23 -0600

    feat: Agregar funcionalidad de creación de suscripciones en la ruta de servicios

--- src/app/views/admin/services/services-routing.module.ts
+++ src/app/views/admin/services/services-routing.module.ts
@@ -4,13 +4,13 @@ import {IndexPackageComponent} from "./packages/index/indexPackage.component";
 import {IndexSubscriptionComponent} from "./subscriptions/index/indexSubscription.component";
 import {authGuard} from "@data/Guards/Auth.guard";
 import {IndexInvoiceComponent} from "@views/admin/services/invoices/index/indexInvoice.component";
-import {CreateInvoiceComponent} from "@views/admin/services/invoices/create/createInvoice.component";
+import {CreateSubscriptionComponent} from "@views/admin/services/subscriptions/create/createSubscription.component";
 
 const routes: Routes = [
 	{path: 'services/packages', component: IndexPackageComponent, canActivate: [authGuard], data: {permissions: 'packages.index'}},
 	{path: 'services/subscriptions', component: IndexSubscriptionComponent, canActivate: [authGuard], data: {permissions: 'subscriptions.index'}},
+	{path: 'services/subscriptions/create', component: CreateSubscriptionComponent, canActivate: [authGuard], data: {permissions: 'subscriptions.create'}},
 	{path: 'services/invoices', component: IndexInvoiceComponent, canActivate: [authGuard], data: {permissions: 'invoices.index'}},
-	{path: 'services/invoices/create', component: CreateInvoiceComponent, canActivate: [authGuard], data: {permissions: 'invoices.create'}},
 ];
 
 @NgModule({
