02e505e537de0714b03d8a289965345f79434ba2
Jesús Avelar <wisusdev@gmail.com>
2024-08-27 21:51:42 -0600

    feat: Agregar componente ShowInvoiceComponent para mostrar detalles de facturas

--- src/app/views/admin/services/services-routing.module.ts
+++ src/app/views/admin/services/services-routing.module.ts
@@ -5,12 +5,14 @@ import {IndexSubscriptionComponent} from "./subscriptions/index/indexSubscriptio
 import {authGuard} from "@data/Guards/Auth.guard";
 import {IndexInvoiceComponent} from "@views/admin/services/invoices/index/indexInvoice.component";
 import {CreateSubscriptionComponent} from "@views/admin/services/subscriptions/create/createSubscription.component";
+import {ShowInvoiceComponent} from "@views/admin/services/invoices/show/showInvoice.component";
 
 const routes: Routes = [
 	{path: 'services/packages', component: IndexPackageComponent, canActivate: [authGuard], data: {permissions: 'packages.index'}},
 	{path: 'services/subscriptions', component: IndexSubscriptionComponent, canActivate: [authGuard], data: {permissions: 'subscriptions.index'}},
 	{path: 'services/subscriptions/create', component: CreateSubscriptionComponent, canActivate: [authGuard], data: {permissions: 'subscriptions.create'}},
 	{path: 'services/invoices', component: IndexInvoiceComponent, canActivate: [authGuard], data: {permissions: 'invoices.index'}},
+	{path: 'services/invoices/show/:id', component: ShowInvoiceComponent, canActivate: [authGuard], data: {permissions: 'invoices.show'}},
 ];
 
 @NgModule({
