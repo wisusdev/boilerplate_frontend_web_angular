234c5e57e546840083c9095fe24d9c2e8517b545
Jesús Avelar <wisusdev@gmail.com>
2024-08-29 21:37:57 -0600

    feat: Add lazy loading for images in ShowInvoiceComponent

--- src/app/views/admin/services/invoices/show/showInvoice.component.ts
+++ src/app/views/admin/services/invoices/show/showInvoice.component.ts
@@ -4,17 +4,26 @@ import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {ServicesService} from "@views/admin/services/services.service";
 import {catchError, of, tap} from "rxjs";
 import {ActivatedRoute} from "@angular/router";
+import {
+	InvoiceAttributes, InvoiceItem, InvoiceUser,
+	ShowInvoiceResponseInterface
+} from "@data/Interfaces/Responses/showInvoiceResponse.interface";
+import {NgForOf} from "@angular/common";
 
 @Component({
 	selector: 'app-show-invoices',
 	standalone: true,
 	imports: [
-		TranslateModule
+		TranslateModule,
+		NgForOf
 	],
 	templateUrl: './showInvoice.component.html'
 })
 export class ShowInvoiceComponent implements OnInit {
 	invoiceId: string = '';
+	invoice: InvoiceAttributes = {} as InvoiceAttributes;
+	invoiceUser: InvoiceUser = {} as InvoiceUser;
+	invoiceItems: InvoiceItem[] = [];
 
 	constructor(
 		private toast: ToastService,
@@ -25,19 +34,21 @@ export class ShowInvoiceComponent implements OnInit {
 	}
 
 	ngOnInit() {
-
 		this.route.params.subscribe(params => {
-			this.invoiceId = params['id'];
-			console.log(this.invoiceId);
+			this.getInvoice(params['id']);
 		});
-
-		this.getInvoice(this.invoiceId);
 	}
 
 	getInvoice(invoiceId: string) {
 		this.services.showInvoice(invoiceId).pipe(
 			tap((response) => {
-				console.log(response);
+				this.invoice = response.data.attributes;
+				this.invoiceItems = response.data.relationships.items;
+				this.invoiceUser = response.data.relationships.user;
+
+				console.log(this.invoice);
+				console.log(this.invoiceItems);
+				console.log(this.invoiceUser);
 			}),
 			catchError((error) => {
 				this.toast.danger(this.translate.instant('errorAsOccurred'));
