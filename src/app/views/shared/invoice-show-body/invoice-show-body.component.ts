1c5bae8a7089df83297842ae295c788d06fd7d62
Jesús Avelar <wisusdev@gmail.com>
2024-09-03 21:29:20 -0600

    feat: Add invoice show body component with HTML and TypeScript files

---
+++ src/app/views/shared/invoice-show-body/invoice-show-body.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input} from '@angular/core';
+import {NgForOf, SlicePipe, TitleCasePipe} from "@angular/common";
+import {TranslateModule} from "@ngx-translate/core";
+
+@Component({
+	selector: 'app-invoice-show-body',
+	standalone: true,
+	imports: [
+		NgForOf,
+		SlicePipe,
+		TitleCasePipe,
+		TranslateModule
+	],
+	templateUrl: './invoice-show-body.component.html',
+	styleUrl: './invoice-show-body.component.css'
+})
+export class InvoiceShowBodyComponent {
+	@Input() invoice: any;
+	@Input() invoiceUser: any;
+	@Input() invoiceItems: any;
+}
