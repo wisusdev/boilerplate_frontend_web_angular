a5f782cf7c072521970f6279abf092fb3855b4f5
Jesus Avelar <wisusdev@gmail.com>
2024-10-11 22:48:32 -0600

    refactor: Agregar validación de campos requeridos en PaymentGatewayConfigComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Auth} from '@data/Providers/Auth';
 import {RouteExceptionService} from "@data/Services/route-exception.service";
+import {catchError, of, tap} from "rxjs";
+import {SettingsService} from "@views/admin/settings/settings.service";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
 	selector: 'app-root',
@@ -12,6 +15,7 @@ export class AppComponent implements OnInit {
 	constructor(
 		private authUser: Auth,
 		private routeExceptionService: RouteExceptionService,
+		private settings: SettingsService,
 	) {
 	}
 
@@ -22,9 +26,41 @@ export class AppComponent implements OnInit {
 		this.authUser.status().pipe().subscribe((status: boolean) => {
 			this.loggedIn = status;
 		});
+
+		const app = localStorage.getItem('app') || null;
+		if (app === null) {
+			this.getAppSettings();
+		}
 	}
 
 	exceptionRoute(): boolean {
 		return this.routeExceptionService.exceptionRoute();
+	}
+
+	getAppSettings() {
+		this.settings.getSettings('app').pipe(
+			tap((response) => {
+				const { attributes } = response.data;
+				const { name, url_api, url_frontend, description, email, phone, address, timezone, logo, favicon } = attributes;
+
+				const formValues = {
+					name,
+					url_api,
+					url_frontend,
+					description,
+					email,
+					phone,
+					address,
+					timezone,
+					logo,
+					favicon,
+				};
+
+				localStorage.setItem('app', JSON.stringify(formValues));
+			}),
+			catchError((error) => {
+				return of(null);
+			})
+		).subscribe();
 	}
 }
