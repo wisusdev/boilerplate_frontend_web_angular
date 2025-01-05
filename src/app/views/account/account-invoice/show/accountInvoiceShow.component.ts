0407ae1875ba89d4f8d9a8a830b004a59554f4cc
Jesús Avelar <wisusdev@gmail.com>
2025-01-05 00:52:34 -0600

    feat: Agregar componente AccountInvoiceShow para mostrar y pagar facturas

---
+++ src/app/views/account/account-invoice/show/accountInvoiceShow.component.ts
@@ -0,0 +1,59 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ServicesService} from "@views/services/services.service";
+import {catchError, of, tap} from "rxjs";
+import {ToastService} from "@data/services/toast.service";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {InvoiceShowBodyComponent} from "@views/shared/invoice-show-body/invoice-show-body.component";
+import {InvoiceAttributes, InvoiceItem, InvoiceUser} from "@data/interfaces/responses/showInvoiceResponse.interface";
+import {TitleCasePipe} from "@angular/common";
+
+@Component({
+	selector: 'app-account-invoice-show',
+	standalone: true,
+	imports: [
+		InvoiceShowBodyComponent,
+		TranslateModule,
+		TitleCasePipe
+	],
+	templateUrl: './accountInvoiceShow.component.html'
+})
+export class AccountInvoiceShowComponent implements OnInit {
+	invoiceId: string = '';
+	invoice: InvoiceAttributes = {} as InvoiceAttributes;
+	invoiceUser: InvoiceUser = {} as InvoiceUser;
+	invoiceItems: InvoiceItem[] = [];
+
+	constructor(
+		protected translate: TranslateService,
+		private toast: ToastService,
+		private route: ActivatedRoute,
+		private services: ServicesService,
+	) {
+	}
+
+	ngOnInit() {
+		this.route.params.subscribe(params => {
+			this.getInvoice(params['id']);
+		});
+	}
+
+	getInvoice(invoiceId: string) {
+		this.services.showInvoice(invoiceId).pipe(
+			tap((response) => {
+				this.invoiceId = response.data.id;
+				this.invoice = response.data.attributes;
+				this.invoiceItems = response.data.relationships.items;
+				this.invoiceUser = response.data.relationships.user;
+			}),
+			catchError((error) => {
+				this.toast.danger(this.translate.instant('errorAsOccurred'));
+				return of(null);
+			})
+		).subscribe();
+	}
+
+	payInvoice() {
+		console.log(this.invoiceId);
+	}
+}
