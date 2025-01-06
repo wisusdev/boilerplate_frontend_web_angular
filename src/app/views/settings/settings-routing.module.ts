03d264921f59e048d13f0842c4b3b307a4025e5d
Jesús Avelar <wisusdev@gmail.com>
2025-01-05 21:02:38 -0600

    fix: renombrar el guard de permisos para seguir la convención de nomenclatura

--- src/app/views/settings/settings-routing.module.ts
+++ src/app/views/settings/settings-routing.module.ts
@@ -9,23 +9,23 @@ import {EditRolComponent} from "./roles/edit/editRol.component";
 import { ShowUserComponent } from "./users/show/show.component";
 import {authGuard} from "@data/guards/auth.guard";
 import {AppConfigComponent} from "@views/settings/config/app/appConfig.component";
-import {PermissionGuard} from "@data/guards/permission.guard";
-import { MailConfigComponent } from "./config/mail/mailConfig.component";
+import {permissionGuard} from "@data/guards/permission.guard";
+import {MailConfigComponent} from "./config/mail/mailConfig.component";
 import {PaymentGatewayConfigComponent} from "@views/settings/config/payment-gateway/payment-gateway-config.component";
 
 const router: Routes = [
-	{path: 'settings/users', component: IndexUserComponent, canActivate: [authGuard, PermissionGuard], data: {permissions: 'users:index'}},
-	{path: 'settings/users/create', component: CreateUserComponent, canActivate: [authGuard, PermissionGuard], data: {permissions: 'users:create'}},
-	{path: 'settings/users/show/:id', component: ShowUserComponent, canActivate: [authGuard, PermissionGuard], data: {permissions: 'users:show'}},
-	{path: 'settings/users/edit/:id', component: EditUserComponent, canActivate: [authGuard, PermissionGuard], data: {permissions: 'users:edit'}},
+	{path: 'settings/users', component: IndexUserComponent, canActivate: [authGuard, permissionGuard], data: {permissions: 'users:index'}},
+	{path: 'settings/users/create', component: CreateUserComponent, canActivate: [authGuard, permissionGuard], data: {permissions: 'users:create'}},
+	{path: 'settings/users/show/:id', component: ShowUserComponent, canActivate: [authGuard, permissionGuard], data: {permissions: 'users:show'}},
+	{path: 'settings/users/edit/:id', component: EditUserComponent, canActivate: [authGuard, permissionGuard], data: {permissions: 'users:edit'}},
 
-	{path: 'settings/roles', component: IndexRolComponent, canActivate: [authGuard, PermissionGuard], data: {permissions: 'roles:index'}},
-	{path: 'settings/roles/create', component: CreateRolComponent, canActivate: [authGuard, PermissionGuard], data: {permissions: 'roles:create'}},
-	{path: 'settings/roles/edit/:id', component: EditRolComponent, canActivate: [authGuard, PermissionGuard], data: {permissions: 'roles:edit'}},
+	{path: 'settings/roles', component: IndexRolComponent, canActivate: [authGuard, permissionGuard], data: {permissions: 'roles:index'}},
+	{path: 'settings/roles/create', component: CreateRolComponent, canActivate: [authGuard, permissionGuard], data: {permissions: 'roles:create'}},
+	{path: 'settings/roles/edit/:id', component: EditRolComponent, canActivate: [authGuard, permissionGuard], data: {permissions: 'roles:edit'}},
 
-	{path: 'settings/app', component: AppConfigComponent, canActivate: [authGuard, PermissionGuard], data: {permissions: 'settings:app'}},
-	{path: 'settings/mail', component: MailConfigComponent, canActivate: [authGuard, PermissionGuard], data: {permissions: 'settings:mail'}},
-	{path: 'settings/payment_gateway', component: PaymentGatewayConfigComponent, canActivate: [authGuard, PermissionGuard], data: {permissions: 'settings:payment_gateway'}}
+	{path: 'settings/app', component: AppConfigComponent, canActivate: [authGuard, permissionGuard], data: {permissions: 'settings:app'}},
+	{path: 'settings/mail', component: MailConfigComponent, canActivate: [authGuard, permissionGuard], data: {permissions: 'settings:mail'}},
+	{path: 'settings/payment_gateway', component: PaymentGatewayConfigComponent, canActivate: [authGuard, permissionGuard], data: {permissions: 'settings:payment_gateway'}}
 ];
 
 @NgModule({
