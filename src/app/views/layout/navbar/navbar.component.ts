8a50de7c46d1463e523c3c3fc9ee66ccfaa4a0ff
Jesús Avelar <wisusdev@gmail.com>
2025-08-10 23:02:39 -0600

    fix: corregir la ruta de importación del servicio de excepciones de ruta

--- src/app/views/layout/navbar/navbar.component.ts
+++ src/app/views/layout/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import {DOCUMENT, NgForOf} from "@angular/common";
 import {catchError, of, tap} from "rxjs";
 import {Auth} from "@data/Providers/auth";
 import {AuthService} from "@views/auth/auth.service";
-import {RouteExceptionService} from "@data/Services/route-exception.service";
+import {RouteExceptionService} from "@data/services/route-exception.service";
 import {environment} from "@env/environment";
 
 @Component({
