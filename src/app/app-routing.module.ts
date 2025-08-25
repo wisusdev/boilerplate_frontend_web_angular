5f4f9ffeab4d7af8521880eb15d2f50452dc5e4e
Jesús Avelar <wisusdev@gmail.com>
2025-08-25 10:58:52 -0600

    refactor: remove pay-package and payment-status components along with their associated templates and services
    - Deleted PayPackageComponent and its template, which handled payment processing for services.
    - Removed PaymentStatusComponent and its template, which displayed payment status messages.
    - Eliminated ServicesRoutingModule and ServicesService, which managed routing and service-related API calls.
    - Removed createSubscription component and its template, which facilitated invoice creation for subscriptions.
    - Deleted indexSubscription component and its template, which listed subscriptions.
    - Removed showSubscription component and its template, which displayed details of a specific subscription.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {authGuard} from '@data/guards/auth.guard';
 import {HomeComponent} from "@views/home/home.component";
-import {WelcomeComponent} from "@views/welcome/welcome.component";
 import {AccountRoutingModule} from "@views/account/account-routing.module";
 import {SettingsRoutingModule} from "@views/settings/settings-routing.module";
 import {AuthRoutingModule} from "@views/auth/auth-routing.module";
-import {ServicesRoutingModule} from "@views/services/services-routing.module";
-import { PublicPackagesComponent } from '@views/services/packages/public-package/public-packages.component';
-import {PayPackageComponent} from "@views/services/pay-package/pay-package.component";
-import {PaymentStatusComponent} from "@views/services/payment-status/payment-status.component";
+import {LoginComponent} from "@views/auth/login/login.component";
 
 const routes: Routes = [
-	{path: '', component: WelcomeComponent, data: {title: 'Welcome'}},
+	{path: '', component: LoginComponent, data: {title: 'Login'}},
 	{path: 'home', component: HomeComponent, canActivate: [authGuard], data: {title: 'Home'}},
-	{path: 'plans', component: PublicPackagesComponent},
-	{path: 'plans/:id', component: PayPackageComponent},
-	{path: 'payment-success/:id', component: PaymentStatusComponent, canActivate: [authGuard]},
-	{path: 'payment-cancelled/:id', component: PaymentStatusComponent, canActivate: [authGuard]},
 	{path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
@@ -26,8 +18,7 @@ const routes: Routes = [
 		RouterModule.forRoot(routes),
 		AccountRoutingModule,
 		AuthRoutingModule,
-		SettingsRoutingModule,
-		ServicesRoutingModule
+		SettingsRoutingModule
 	],
 	exports: [RouterModule]
 })
