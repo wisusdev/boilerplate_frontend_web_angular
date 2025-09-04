f7260d134e1f15b3165c686eb8af72cc552895d4
Jesús Avelar <wisusdev@gmail.com>
2025-09-03 21:33:23 -0600

    refactor: simplificar importaciones de interfaces en AuthService

--- src/app/views/auth/auth.service.ts
+++ src/app/views/auth/auth.service.ts
@@ -4,68 +4,68 @@ import {Observable} from "rxjs";
 import {catchError} from "rxjs/operators";
 import {Handle} from "@data/Exceptions/handle";
 import {environment} from "@env/environment";
-import {LoginResponseInterface} from "@data/interfaces/responses/loginResponse.interface";
-import {LoginRequestInterface} from "@data/interfaces/requests/loginRequest.interface";
-import {RegisterRequestInterface} from "@data/interfaces/requests/registerRequest.interface";
-import {RegisterResponseInterface} from "@data/interfaces/responses/registerResponse.interface";
-import {ForgotPasswordRequestInterface} from "@data/interfaces/requests/forgotPasswordRequest.interface";
-import {ForgotPasswordResponseInterface} from "@data/interfaces/responses/forgotPasswordResponse.interface";
-import {ResetPasswordRequestInterface} from "@data/interfaces/requests/resetPasswordRequest.interface";
-import {ResetPasswordResponseInterface} from "@data/interfaces/responses/resetPasswordResponse.interface";
+import {
+    LoginResponse,
+    LoginRequest,
+    RegisterRequest,
+    RegisterResponse,
+    ForgotPasswordRequest,
+    ForgotPasswordResponse,
+    ResetPasswordRequest,
+    ResetPasswordResponse
+} from "@data/interfaces";
 
 @Injectable({
-	providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-	private _apiUri = environment.api_url_v1;
-	private _apiUriLogin = this._apiUri + '/auth/login';
-	private _apiUriLogout = this._apiUri + '/auth/logout';
-	private _apiUriRefresh = this._apiUri + '/auth/refresh';
-	private _apiUriRegister = this._apiUri + '/auth/register';
-	private _apiUriForgotPassword = this._apiUri + '/auth/forgot-password';
-	private _apiUriResetPassword = this._apiUri + '/auth/reset-password';
+    private _apiUri = environment.api_url_v1;
+    private _apiUriLogin = this._apiUri + '/auth/login';
+    private _apiUriLogout = this._apiUri + '/auth/logout';
+    private _apiUriRefresh = this._apiUri + '/auth/refresh';
+    private _apiUriRegister = this._apiUri + '/auth/register';
+    private _apiUriForgotPassword = this._apiUri + '/auth/forgot-password';
+    private _apiUriResetPassword = this._apiUri + '/auth/reset-password';
 
-	constructor(
-		private httpClient: HttpClient,
-		private handleMessage: Handle
-	) {
-	}
+    constructor(private httpClient: HttpClient, private handleMessage: Handle) {
 
-	httpHeaders: HttpHeaders = new HttpHeaders(environment.headers);
+    }
 
-	login(data: LoginRequestInterface): Observable<LoginResponseInterface> {
-		return this.httpClient.post<LoginResponseInterface>(`${this._apiUriLogin}`, data, {
-			headers: this.httpHeaders,
-		}).pipe(catchError(this.handleMessage.errorHandle));
-	}
+    httpHeaders: HttpHeaders = new HttpHeaders(environment.headers);
 
-	register(data: RegisterRequestInterface): Observable<RegisterResponseInterface> {
-		return this.httpClient.post<RegisterResponseInterface>(`${this._apiUriRegister}`, data, {
-			headers: this.httpHeaders,
-		}).pipe(catchError(this.handleMessage.errorHandle));
-	}
+    login(data: LoginRequest): Observable<LoginResponse> {
+        return this.httpClient.post<LoginResponse>(`${this._apiUriLogin}`, data, {
+            headers: this.httpHeaders,
+        }).pipe(catchError(this.handleMessage.errorHandle));
+    }
 
-	logout(): Observable<object> {
-		return this.httpClient.post(`${this._apiUriLogout}`, {}, {
-			headers: this.httpHeaders,
-		}).pipe(catchError(this.handleMessage.errorHandle));
-	}
+    register(data: RegisterRequest): Observable<RegisterResponse> {
+        return this.httpClient.post<RegisterResponse>(`${this._apiUriRegister}`, data, {
+            headers: this.httpHeaders,
+        }).pipe(catchError(this.handleMessage.errorHandle));
+    }
 
-	refreshToken(): Observable<object> {
-		return this.httpClient.post(`${this._apiUriRefresh}`, {
-			headers: this.httpHeaders,
-		}).pipe(catchError(this.handleMessage.errorHandle));
-	}
+    logout(): Observable<object> {
+        return this.httpClient.post(`${this._apiUriLogout}`, {}, {
+            headers: this.httpHeaders,
+        }).pipe(catchError(this.handleMessage.errorHandle));
+    }
 
-	forgotPassword(data: ForgotPasswordRequestInterface): Observable<ForgotPasswordResponseInterface> {
-		return this.httpClient.post<ForgotPasswordResponseInterface>(`${this._apiUriForgotPassword}`, data, {
-			headers: this.httpHeaders,
-		}).pipe(catchError(this.handleMessage.errorHandle));
-	}
+    refreshToken(): Observable<object> {
+        return this.httpClient.post(`${this._apiUriRefresh}`, {
+            headers: this.httpHeaders,
+        }).pipe(catchError(this.handleMessage.errorHandle));
+    }
 
-	resetPassword(data: ResetPasswordRequestInterface): Observable<ResetPasswordResponseInterface> {
-		return this.httpClient.post<ResetPasswordResponseInterface>(`${this._apiUriResetPassword}`, data, {
-			headers: this.httpHeaders,
-		}).pipe(catchError(this.handleMessage.errorHandle));
-	}
+    forgotPassword(data: ForgotPasswordRequest): Observable<ForgotPasswordResponse> {
+        return this.httpClient.post<ForgotPasswordResponse>(`${this._apiUriForgotPassword}`, data, {
+            headers: this.httpHeaders,
+        }).pipe(catchError(this.handleMessage.errorHandle));
+    }
+
+    resetPassword(data: ResetPasswordRequest): Observable<ResetPasswordResponse> {
+        return this.httpClient.post<ResetPasswordResponse>(`${this._apiUriResetPassword}`, data, {
+            headers: this.httpHeaders,
+        }).pipe(catchError(this.handleMessage.errorHandle));
+    }
 }
